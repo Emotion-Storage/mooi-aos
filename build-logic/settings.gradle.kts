88d8fd99715e95994a5849a85391e03a80b512d5
sunjoolee <sheisthepoem@naver.com>
2025-07-16 14:54:56 +0900

    fix: remove build-logic build script & add convention module

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,5 +1,9 @@
-@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
     versionCatalogs {
         create("libs") {
             from(files("../gradle/libs.versions.toml"))
