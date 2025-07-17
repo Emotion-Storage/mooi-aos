253108c9203be04ba4b07941b9efd534a4fd1dda
sunjoolee <sheisthepoem@naver.com>
2025-07-17 13:28:16 +0900

    build: add aplugins in AndroidApplicationComposeConventionPlugin.kt

--- build-logic/convention/src/main/java/com/emotionstorage/convention/AndroidApplicationComposeConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/emotionstorage/convention/AndroidApplicationComposeConventionPlugin.kt
@@ -6,6 +6,12 @@ import org.gradle.api.Project
 class AndroidApplicationComposeConventionPlugin : Plugin<Project>{
     override fun apply(target: Project) {
         println("*** AndroidAppComposeConventionPlugin invoked ***")
-        // Additional configuration here...
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+                apply("org.jetbrains.kotlin.plugin.compose")
+            }
+        }
     }
 }
