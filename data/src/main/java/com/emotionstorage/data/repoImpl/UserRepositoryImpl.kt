876866611e12369549ee3a74952c86dd302dc1e6
sunjoolee <sheisthepoem@naver.com>
2025-07-27 15:53:42 +0900

    feat: add user save & deletion logic in user repo

--- data/src/main/java/com/emotionstorage/data/repoImpl/UserRepositoryImpl.kt
+++ data/src/main/java/com/emotionstorage/data/repoImpl/UserRepositoryImpl.kt
@@ -9,9 +9,18 @@ import javax.inject.Inject
 class UserRepositoryImpl @Inject constructor(
     private val userLocalDataSource: UserLocalDataSource
 ) : UserRepository {
+    override suspend fun saveUser(user: User): Boolean {
+        return userLocalDataSource.saveUser(UserMapper.toData(user))
+    }
+
     override suspend fun getUser(): User? {
         return userLocalDataSource.getUser()?.run {
             UserMapper.toDomain(this)
         }
+        // todo: get user from remote if null
+    }
+
+    override suspend fun deleteUser(): Boolean {
+        return userLocalDataSource.deleteUser()
     }
 }
--- domain/src/main/java/com/emotionstorage/domain/repo/UserRepository.kt
+++ domain/src/main/java/com/emotionstorage/domain/repo/UserRepository.kt
@@ -3,5 +3,7 @@ package com.emotionstorage.domain.repo
 import com.emotionstorage.domain.model.User
 
 interface UserRepository {
+    suspend fun saveUser(user:User): Boolean
     suspend fun getUser(): User?
+    suspend fun deleteUser(): Boolean
 }
