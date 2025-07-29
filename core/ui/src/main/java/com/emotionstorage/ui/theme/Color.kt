e4eb88790045fbdbd7bc0fe528bb5ef92e5bfb88
sunjoolee <sheisthepoem@naver.com>
2025-07-29 21:09:37 +0900

    feat: config custom color scheme in theme

--- core/ui/src/main/java/com/emotionstorage/ui/theme/Color.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/theme/Color.kt
@@ -1,11 +1,24 @@
 package com.emotionstorage.ui.theme
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+@Immutable
+data class MooiColorScheme(
+    val primary: Color = Color(0xFFAFCBFA),
+    val secondary: Color = Color(0xFFF5CFE1),
+    val tertiary: Color = Color(0xFF859CEA),
+    val background: Color = Color(0xFF1E1C24),
+    val bottomBarBackground: Color = Color(0xFF100E14),
+    val errorRed: Color = Color(0xFFF36969),
+    val gray900: Color = Color(0xFF1E1E1E),
+    val gray800: Color = Color(0xFF3D3D3D),
+    val gray700: Color = Color(0xFF5C5C5C),
+    val gray600: Color = Color(0xFF6F6F6F),
+    val gray500: Color = Color(0xFF989898),
+    val gray400: Color = Color(0xFFB7B8B8),
+    val gray300: Color = Color(0xFFDADADA),
+    val gray200: Color = Color(0xFFEAEAEA),
+    val gray100: Color = Color(0xFFF3F3F3),
+    val gray50: Color = Color(0xFFF9F9F9),
+)
