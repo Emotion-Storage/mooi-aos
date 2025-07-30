5b18b7da6c08ddaea9cd352d6653a25e0cbd5a32
sunjoolee <sheisthepoem@naver.com>
2025-07-30 10:32:34 +0900

    feat: add provider icons in login button

--- feat/auth/src/main/java/com/emotionstorage/auth/ui/component/SocialLoginButton.kt
+++ feat/auth/src/main/java/com/emotionstorage/auth/ui/component/SocialLoginButton.kt
@@ -1,7 +1,12 @@
 package com.emotionstorage.auth.ui.component
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -10,11 +15,13 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.emotionstorage.domain.model.User.AuthProvider
 import com.emotionstorage.ui.theme.MooiTheme
 import com.emotionstorage.auth.R
@@ -25,7 +32,6 @@ fun SocialLoginButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit
 ) {
-    // todo: add social provider icon
     Button(
         modifier = modifier
             .width(328.dp)
@@ -39,14 +45,29 @@ fun SocialLoginButton(
                 AuthProvider.GOOGLE -> Color.White
             },
             contentColor = Color(0xFF1A1802)
-        )
+        ),
+        contentPadding = PaddingValues(0.dp)
     ) {
-        Text(
-            style = MooiTheme.typography.button,
-            text =
-                if (provider == AuthProvider.KAKAO) stringResource(R.string.login_btn_kako)
-                else stringResource(R.string.login_btn_google)
-        )
+        Box(modifier = Modifier.fillMaxSize()) {
+            AsyncImage(
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+                    .height(21.dp)
+                    .padding(start = 24.dp),
+                model = when (provider) {
+                    AuthProvider.KAKAO -> R.drawable.kakao_logo
+                    AuthProvider.GOOGLE -> R.drawable.google_logo
+                },
+                contentDescription = null
+            )
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                style = MooiTheme.typography.button,
+                text =
+                    if (provider == AuthProvider.KAKAO) stringResource(R.string.login_btn_kako)
+                    else stringResource(R.string.login_btn_google)
+            )
+        }
     }
 }
 
