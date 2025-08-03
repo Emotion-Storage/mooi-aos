9fe63f93e752b4b410a26e549959dd83c92b0046
sunjoolee <sheisthepoem@naver.com>
2025-08-03 14:35:00 +0900

    fix: add LocalDateTime type converter for room

--- core/local/src/main/java/com/emotionstorage/local/room/DtoConverter.kt
+++ core/local/src/main/java/com/emotionstorage/local/room/DtoConverter.kt
@@ -2,10 +2,18 @@ package com.emotionstorage.local.room
 
 import androidx.room.TypeConverter
 import java.time.LocalDate
+import java.time.LocalDateTime
+
 class DtoConverter {
     @TypeConverter
     fun fromLocalDate(date: LocalDate?): String? = date?.toString()
 
     @TypeConverter
     fun toLocalDate(value: String?): LocalDate? = value?.let { LocalDate.parse(it) }
+
+    @TypeConverter
+    fun fromLocalDataTime(date: LocalDateTime?): String? = date?.toString()
+
+    @TypeConverter
+    fun toLocalDateTime(value: String?): LocalDateTime? = value?.let { LocalDateTime.parse(it) }
 }
