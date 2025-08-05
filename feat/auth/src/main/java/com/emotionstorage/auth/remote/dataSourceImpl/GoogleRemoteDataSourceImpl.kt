6dea14b2c9457ab5fdf75c608b6ac0d484711b87
sunjoolee <sheisthepoem@naver.com>
2025-08-05 15:12:52 +0900

    fix: fix credential option for google signin button

--- feat/auth/src/main/java/com/emotionstorage/auth/remote/dataSourceImpl/GoogleRemoteDataSourceImpl.kt
+++ feat/auth/src/main/java/com/emotionstorage/auth/remote/dataSourceImpl/GoogleRemoteDataSourceImpl.kt
@@ -2,19 +2,20 @@ package com.emotionstorage.auth.remote.dataSourceImpl
 
 import android.content.Context
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.credentials.CredentialManager
 import androidx.credentials.CustomCredential
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.GetCredentialResponse
-import com.emotionstorage.auth.BuildConfig
 import com.emotionstorage.auth.data.dataSource.GoogleRemoteDataSource
 import com.emotionstorage.common.DataResource
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
+import com.emotionstorage.auth.BuildConfig
 
 @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 class GoogleRemoteDataSourceImpl @Inject constructor(
@@ -25,34 +26,42 @@ class GoogleRemoteDataSourceImpl @Inject constructor(
     }
 
     override suspend fun getIdToken(): DataResource<String> {
-        val credentialResult: GetCredentialResponse = credentialManager.getCredential(
-            request = GetCredentialRequest
-                .Builder()
-                .addCredentialOption(
-                    GetGoogleIdOption
-                        .Builder()
-                        .setFilterByAuthorizedAccounts(true)
-                        .setServerClientId(BuildConfig.GOOGLE_CLIENT_ID)
-                        .setAutoSelectEnabled(false)
-                        .build()
-                )
-                .build(),
-            context = context,
-        )
+        try {
+            val credentialResult: GetCredentialResponse = credentialManager.getCredential(
+                request = GetCredentialRequest.Builder().addCredentialOption(
+                    GetSignInWithGoogleOption
+                        .Builder(
+                            serverClientId = BuildConfig.GOOGLE_CLIENT_ID
+                        ).build()
+                ).build(),
+                context = context,
+            )
 
-        credentialResult.credential.apply {
-            if (this is CustomCredential && this.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
-                try {
-                    val googleIdTokenCredential = GoogleIdTokenCredential
-                        .createFrom(this.data)
+            // todo: fix code not being executed below
+            Log.d("GoogleRemoteDataSourceImpl", "credential response: $credentialResult")
 
-                    return DataResource.Success(googleIdTokenCredential.idToken)
-                } catch (e: GoogleIdTokenParsingException) {
-                    return DataResource.Error(e)
+            credentialResult.credential.apply {
+                if (this is CustomCredential && this.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+                    try {
+                        val googleIdTokenCredential = GoogleIdTokenCredential
+                            .createFrom(this.data)
+
+                        Log.d(
+                            "GoogleRemoteDataSourceImpl",
+                            "getIdToken: ${googleIdTokenCredential.idToken}"
+                        )
+                        return DataResource.Success(googleIdTokenCredential.idToken)
+                    } catch (e: GoogleIdTokenParsingException) {
+                        return DataResource.Error(e)
+                    }
+                } else {
+                    Log.e("GoogleRemoteDataSourceImpl", "getIdToken: Unexpected type of credential")
+                    return DataResource.Error(Exception("Unexpected type of credential"))
                 }
-            } else {
-                return DataResource.Error(Exception("Unexpected type of credential"))
             }
+        } catch (e: Exception){
+            Log.e("GoogleRemoteDataSourceImpl", "credential response error: $e")
+            return DataResource.Error(e)
         }
     }
 }
