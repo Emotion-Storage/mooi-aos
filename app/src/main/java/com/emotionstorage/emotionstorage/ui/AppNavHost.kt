f0ce328f9c0728782e25db05f3774218f9985a8a
sunjoolee <sheisthepoem@naver.com>
2025-09-01 20:03:11 +0900

    fix: fix nav stack clearing logic

--- app/src/main/java/com/emotionstorage/emotionstorage/ui/AppNavHost.kt
+++ app/src/main/java/com/emotionstorage/emotionstorage/ui/AppNavHost.kt
@@ -5,11 +5,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navOptions
 import androidx.navigation.toRoute
 import com.emotionstorage.auth.ui.LoginScreen
 import com.emotionstorage.domain.model.User.AuthProvider
@@ -42,10 +40,14 @@ internal sealed class AppDestination {
     object Home : AppDestination()
 }
 
-// clear stack, including splash
-internal fun NavOptionsBuilder.clearStack(){
-    popUpTo(AppDestination.Splash) {
-        inclusive = true
+// navigate to destination with clear stack
+internal fun <T : Any> NavHostController.navigateWithClearStack(destRoute: T) {
+    val currentRoute = currentBackStackEntry?.destination?.route
+
+    this.navigate(destRoute) {
+        popUpTo(currentRoute ?: destRoute.toString()) {
+            inclusive = true
+        }
     }
 }
 
@@ -54,13 +56,6 @@ internal fun AppNavHost(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier,
 ) {
-    val navToHome = {
-        navController.navigate(AppDestination.Home) {
-            // always clear back stack when navigating to home
-            clearStack()
-        }
-    }
-
 
     NavHost(
         navController,
@@ -72,25 +67,27 @@ internal fun AppNavHost(
         composable<AppDestination.Splash> { backstackEntry ->
             SplashScreen(
                 navToTutorial = {
-                    navController.navigate(AppDestination.Tutorial) {
-                        clearStack()
-                    }
+                    navController.navigateWithClearStack(AppDestination.Tutorial)
                 },
-                navToHome = navToHome
+                navToHome = {
+                    navController.navigateWithClearStack(AppDestination.Home)
+                }
             )
         }
 
         composable<AppDestination.Tutorial> { backstackEntry ->
             TutorialScreen(
                 navToLogin = {
-                    navController.navigate(AppDestination.Login)
+                    navController.navigateWithClearStack(AppDestination.Login)
                 }
             )
         }
 
         composable<AppDestination.Login> { backstackEntry ->
             LoginScreen(
-                navToHome = navToHome,
+                navToHome =  {
+                    navController.navigateWithClearStack(AppDestination.Home)
+                },
                 navToOnBoarding = { provider, idToken ->
                     navController.navigate(AppDestination.OnBoarding(provider.toString(), idToken))
                 }
@@ -100,12 +97,14 @@ internal fun AppNavHost(
         composable<AppDestination.OnBoarding> { backstackEntry ->
             val arguments = backstackEntry.toRoute<AppDestination.OnBoarding>()
             OnBoardingNavHost(
-                navToLogin = {
-                    navController.navigate(AppDestination.Login)
-                },
-                navToHome = navToHome,
                 idToken = arguments.idToken,
-                provider = AuthProvider.valueOf(arguments.provider)
+                provider = AuthProvider.valueOf(arguments.provider),
+                navToLogin = {
+                    navController.navigateWithClearStack(AppDestination.Login)
+                },
+                navToHome =  {
+                    navController.navigateWithClearStack(AppDestination.Home)
+                },
             )
         }
 
