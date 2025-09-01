5f3e0db35c547c4d643f84358f7739d89d0b3892
sunjoolee <sheisthepoem@naver.com>
2025-09-01 22:26:06 +0900

    feat: update auth api

--- core/remote/src/main/java/com/emotionstorage/remote/response/ResponseDto.kt
+++ core/remote/src/main/java/com/emotionstorage/remote/response/ResponseDto.kt
@@ -1,10 +1,15 @@
 package com.emotionstorage.remote.response
 
+import com.emotionstorage.common.LocalDateTimeSerializer
 import kotlinx.serialization.Serializable
+import java.time.LocalDateTime
 
 @Serializable
 data class ResponseDto<T>(
-    val status: ResponseStatus,
+    val status: Int,
+    val code: String,
     val message: String?,
-    val data: T?
+    val data: T?,
+    @Serializable(with = LocalDateTimeSerializer::class)
+    val timestamp: LocalDateTime
 )
