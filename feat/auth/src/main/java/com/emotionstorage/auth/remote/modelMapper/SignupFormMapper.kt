75cca09a1fe98a18c3f874ed0e7663347c4289cc
sunjoolee <sheisthepoem@naver.com>
2025-09-02 11:05:41 +0900

    fix: fix signup request body date format

--- feat/auth/src/main/java/com/emotionstorage/auth/remote/modelMapper/SignupFormMapper.kt
+++ feat/auth/src/main/java/com/emotionstorage/auth/remote/modelMapper/SignupFormMapper.kt
@@ -2,13 +2,14 @@ package com.emotionstorage.auth.remote.modelMapper
 
 import com.emotionstorage.auth.data.model.SignupFormEntity
 import com.emotionstorage.auth.remote.request.SignupRequestBody
+import java.time.format.DateTimeFormatter
 
 internal object SignupFormMapper {
     fun toRemote(signupFormEntity: SignupFormEntity) = SignupRequestBody(
         idToken = signupFormEntity.idToken,
         nickname = signupFormEntity.nickname,
         gender = signupFormEntity.gender,
-        birthday = signupFormEntity.birthday,
+        birthday = signupFormEntity.birthday.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
         expectations = signupFormEntity.expectations,
         isTermAgreed = signupFormEntity.isTermAgreed,
         isPrivacyAgreed = signupFormEntity.isPrivacyAgreed,
--- feat/auth/src/main/java/com/emotionstorage/auth/remote/request/SignupRequestBody.kt
+++ feat/auth/src/main/java/com/emotionstorage/auth/remote/request/SignupRequestBody.kt
@@ -1,16 +1,13 @@
 package com.emotionstorage.auth.remote.request
 
-import com.emotionstorage.common.LocalDateSerializer
 import kotlinx.serialization.Serializable
-import java.time.LocalDate
 
 @Serializable
 data class SignupRequestBody (
     val idToken: String,
     val nickname: String,
     val gender: String,
-    @Serializable(with = LocalDateSerializer::class)
-    val birthday: LocalDate,
+    val birthday: String,
     val expectations: List<String>,
     val isTermAgreed: Boolean,
     val isPrivacyAgreed: Boolean,
