11744395e7466a5fce3d3f748cd5bcb276a8c576
sunjoolee <sheisthepoem@naver.com>
2025-09-09 22:23:20 +0900

    fix: set favoriteAt nullable

--- domain/src/main/java/com/emotionstorage/domain/model/TimeCapsule.kt
+++ domain/src/main/java/com/emotionstorage/domain/model/TimeCapsule.kt
@@ -14,7 +14,7 @@ data class TimeCapsule(
     val logs: List<OpenLog> = emptyList(),
     val createdAt: LocalDateTime, // 생성 시각
     val arriveAt: LocalDateTime? = null, // 도착 시각
-    val favoriteAt: LocalDateTime, // 즐겨찾기 설정 시각
+    val favoriteAt: LocalDateTime? = null, // 즐겨찾기 설정 시각
     val updatedAt: LocalDateTime,
 ) {
     enum class STATUS {
