08982c1311fba18752370e48e3d5e0281f8c6e4a
sunjoolee <sheisthepoem@naver.com>
2025-09-11 00:16:37 +0900

    feat: set main btn gradient color steps

--- core/ui/src/main/java/com/emotionstorage/ui/theme/Brush.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/theme/Brush.kt
@@ -13,8 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.LinearGradient
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -22,29 +24,33 @@ import androidx.compose.ui.unit.dp
 @Immutable
 data class MooiBrushScheme(
     val mainButtonBackground: Brush = Brush.linearGradient(
-        listOf(
-            Color(0xFFAFCBFA),
-            Color(0xFF9BB4F2),
-            Color(0xFF859CEA)
+        colorStops = arrayOf(
+            0.0f to Color(0xFFAFCBFA),
+            0.4f to Color(0xFF9BB4F2),
+            1.0f to Color(0xFF859CEA)
         ),
+        // todo: set degree to -75
     ),
     val subButtonBackground: Brush = Brush.verticalGradient(
         listOf(
             Color(0x1A849BEA),
             Color(0x0D849BEA),
-        )
+        ),
+        // todo: set degree to -9
     ),
     val subButtonBorder: Brush = Brush.horizontalGradient(
         listOf(
             Color(0x66AECBFA),
             Color(0x08AECBFA),
         )
+        // todo: set degree to -42
     ),
     val commentBackground: Brush = Brush.verticalGradient(
         listOf(
             Color(0x80849BEA),
             Color(0x14849BEA)
         )
+        // todo: set degree to -42
     ),
 )
 
@@ -91,6 +97,11 @@ private fun BrushPreview() {
                     .fillMaxWidth()
                     .height(200.dp)
                     .background(MooiTheme.brushScheme.commentBackground, RoundedCornerShape(10.dp))
+                    .border(
+                        width = 1.dp,
+                        brush = MooiTheme.brushScheme.subButtonBorder,
+                        shape = RoundedCornerShape(10.dp)
+                    )
             )
         }
     }
