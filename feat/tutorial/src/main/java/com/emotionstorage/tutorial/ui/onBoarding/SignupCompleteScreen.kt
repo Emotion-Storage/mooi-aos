ebad08ef66fdc7b8bede4d3685a0e0cc80941004
sunjoolee <sheisthepoem@naver.com>
2025-09-11 01:00:59 +0900

    refactor: refactor signup complete screen title

--- feat/tutorial/src/main/java/com/emotionstorage/tutorial/ui/onBoarding/SignupCompleteScreen.kt
+++ feat/tutorial/src/main/java/com/emotionstorage/tutorial/ui/onBoarding/SignupCompleteScreen.kt
@@ -9,11 +9,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import com.emotionstorage.tutorial.R
@@ -37,9 +43,6 @@ fun SignupCompleteScreen(
         modifier = modifier
             .background(MooiTheme.colorScheme.background)
             .fillMaxSize(),
-        topBar = {
-            TopAppBar()
-        }
     ) { padding ->
         Column(
             modifier = Modifier
@@ -50,13 +53,21 @@ fun SignupCompleteScreen(
                 .imePadding(),
             verticalArrangement = Arrangement.SpaceBetween
         ) {
-            OnBoardingTitle(
-                modifier = Modifier.fillMaxWidth(),
-                showSteps = false,
-                title = stringResource(R.string.on_boarding_signup_complete_title),
-                titleHighlights = stringResource(R.string.on_boarding_signup_complete_title_highlights).split(
-                    ','
-                )
+            Text(
+                modifier = Modifier.padding(top = 62.dp),
+                text = buildAnnotatedString {
+                    append("가입을 환영해요.\n이제 ")
+                    withStyle(SpanStyle(color = MooiTheme.colorScheme.primary)) {
+                        append("당신의 이야기")
+                    }
+                    append("를\n")
+                    withStyle(SpanStyle(color = MooiTheme.colorScheme.primary)) {
+                        append("우리만의 공간")
+                    }
+                    append("에 담아보세요.")
+                },
+                style = MooiTheme.typography.head1.copy(fontWeight = FontWeight.SemiBold),
+                color = Color.White
             )
 
             Column(
