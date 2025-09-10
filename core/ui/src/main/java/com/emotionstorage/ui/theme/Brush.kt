0bc08e7f71a38c3f91dd59ddee59f3e6e403792c
sunjoolee <sheisthepoem@naver.com>
2025-09-11 00:29:01 +0900

    feat: set gradient angle in degrees

--- core/ui/src/main/java/com/emotionstorage/ui/theme/Brush.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/theme/Brush.kt
@@ -12,46 +12,44 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.LinearGradient
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.emotionstorage.ui.util.LinearGradient
 
-// todo: adjust gradient offsets
 @Immutable
 data class MooiBrushScheme(
-    val mainButtonBackground: Brush = Brush.linearGradient(
-        colorStops = arrayOf(
-            0.0f to Color(0xFFAFCBFA),
-            0.4f to Color(0xFF9BB4F2),
-            1.0f to Color(0xFF859CEA)
+    val mainButtonBackground: Brush = LinearGradient(
+        colors = listOf(
+            Color(0xFFAFCBFA),
+            Color(0xFF9BB4F2),
+            Color(0xFF859CEA)
         ),
-        // todo: set degree to -75
+        stops = listOf(0.0f, 0.4f, 1.0f),
+        angleInDegrees = -75f,
     ),
-    val subButtonBackground: Brush = Brush.verticalGradient(
-        listOf(
+    val subButtonBackground: Brush = LinearGradient(
+        colors = listOf(
             Color(0x1A849BEA),
             Color(0x0D849BEA),
         ),
-        // todo: set degree to -9
+        angleInDegrees = -9f,
     ),
-    val subButtonBorder: Brush = Brush.horizontalGradient(
-        listOf(
+    val subButtonBorder: Brush = LinearGradient(
+        colors = listOf(
             Color(0x66AECBFA),
             Color(0x08AECBFA),
-        )
-        // todo: set degree to -42
+        ),
+        angleInDegrees = -42f,
     ),
-    val commentBackground: Brush = Brush.verticalGradient(
-        listOf(
+    val commentBackground: Brush = LinearGradient(
+        colors = listOf(
             Color(0x80849BEA),
             Color(0x14849BEA)
-        )
-        // todo: set degree to -42
-    ),
+        ),
+        angleInDegrees = -42f,
+    )
 )
 
 
