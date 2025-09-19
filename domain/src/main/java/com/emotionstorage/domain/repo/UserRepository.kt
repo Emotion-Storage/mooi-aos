66b377ff8dd2cc67913640c0e1a0d4db2951f9c0
sunjoolee <sheisthepoem@naver.com>
2025-09-19 18:33:27 +0900

    feat(user): wrap return type with Flow & DataState

--- domain/src/main/java/com/emotionstorage/domain/repo/UserRepository.kt
+++ domain/src/main/java/com/emotionstorage/domain/repo/UserRepository.kt
@@ -1,9 +1,11 @@
 package com.emotionstorage.domain.repo
 
 import com.emotionstorage.domain.model.User
+import kotlinx.coroutines.flow.Flow
+import com.emotionstorage.domain.common.DataState
 
 interface UserRepository {
     suspend fun saveUser(user:User): Boolean
-    suspend fun getUser(): User?
+    suspend fun getUser(): Flow<DataState<User>>
     suspend fun deleteUser(): Boolean
 }
