ce5cebb6918ddd40db65bf08caa8e907588c271c
sunjoolee <sheisthepoem@naver.com>
2025-09-23 14:28:03 +0900

    feat(toast): add enter/exit animation

--- core/ui/src/main/java/com/emotionstorage/ui/component/Toast.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/component/Toast.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,7 +18,6 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -102,7 +100,7 @@ fun BoxScope.Toast(
 
 @Preview
 @Composable
-private fun CustomToastPreview() {
+private fun ToastPreview() {
     val coroutineScope = rememberCoroutineScope()
     val (showToast, setShowToast) = remember { mutableStateOf(false) }
 
