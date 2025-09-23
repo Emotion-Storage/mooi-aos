ecdcdb28adad08ff02e1611a5f1066679cf48348
sunjoolee <sheisthepoem@naver.com>
2025-09-23 15:13:43 +0900

    fix: fix local date time korean format

--- core/common/src/main/java/com/emotionstorage/common/LocalDateTimeUtil.kt
+++ core/common/src/main/java/com/emotionstorage/common/LocalDateTimeUtil.kt
@@ -4,9 +4,9 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 fun LocalDateTime.formatToKorDateTime(): String {
-    return this.format(DateTimeFormatter.ofPattern("yyyy.MM.dd")) + " " +  this.formatToKorTime()
+    return this.format(DateTimeFormatter.ofPattern("yyyy.MM.dd")) + " " + this.formatToKorTime()
 }
 
 fun LocalDateTime.formatToKorTime(): String {
-    return "${if (this.hour >= 12) "오후" else "오전"} ${if (this.hour > 12) this.hour - 12 else this.hour}:${this.minute}"
+    return (if (this.hour >= 12) "오후" else "오전") + " " + DateTimeFormatter.ofPattern("hh:mm").format(this)
 }
