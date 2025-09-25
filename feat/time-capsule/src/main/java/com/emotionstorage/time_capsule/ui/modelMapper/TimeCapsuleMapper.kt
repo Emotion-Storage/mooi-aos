933283e6a89719a8f4554b5c5553548348eee481
sunjoolee <sheisthepoem@naver.com>
2025-09-25 17:21:08 +0900

    fix: fix time capsule domain -> ui mapper

--- feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/ui/modelMapper/TimeCapsuleMapper.kt
+++ feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/ui/modelMapper/TimeCapsuleMapper.kt
@@ -1,16 +1,21 @@
 package com.emotionstorage.time_capsule.ui.modelMapper
 
-import com.emotionstorage.time_capsule.ui.model.TimeCapsuleState
+import com.emotionstorage.domain.model.TimeCapsule
+import com.emotionstorage.time_capsule.ui.model.TimeCapsuleItemState
+import java.time.LocalDateTime
 
 object TimeCapsuleMapper {
-    fun toUi(domain: TimeCapsuleState): TimeCapsuleState {
-        return TimeCapsuleState(
+    fun toUi(domain: TimeCapsule): TimeCapsuleItemState =
+        TimeCapsuleItemState(
             id = domain.id,
+            status = domain.status,
             title = domain.title,
             emotions = domain.emotions,
             isFavorite = domain.isFavorite,
-            isFavoriteAt = domain.isFavoriteAt,
-            createdAt = domain.createdAt
+            isFavoriteAt = domain.favoriteAt,
+            createdAt = domain.createdAt,
+            openDday = domain.arriveAt?.run {
+                this.dayOfYear - LocalDateTime.now().dayOfYear
+            }
         )
-    }
 }
