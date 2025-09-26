e23dac50064e735e8ecbfb47be896b0caca031ed
sunjoolee <sheisthepoem@naver.com>
2025-09-26 23:03:00 +0900

    feat: add new badge in daily report button

--- feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/ui/component/TimeCapsuleCalendarBottomSheet.kt
+++ feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/ui/component/TimeCapsuleCalendarBottomSheet.kt
@@ -11,8 +11,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,9 +29,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import com.emotionstorage.common.toKorDate
 import com.emotionstorage.domain.model.TimeCapsule.Emotion
 import com.emotionstorage.domain.model.TimeCapsule.STATUS
@@ -36,6 +43,7 @@ import com.emotionstorage.time_capsule.ui.model.TimeCapsuleItemState
 import com.emotionstorage.ui.component.BottomSheet
 import com.emotionstorage.ui.component.CtaButton
 import com.emotionstorage.ui.theme.MooiTheme
+import com.emotionstorage.ui.theme.pretendard
 import com.emotionstorage.ui.util.mainBackground
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -90,9 +98,7 @@ fun TimeCapsuleCalendarBottomSheet(
         }
 
         DailyReportButton(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height((66.33).dp),
+            modifier = Modifier.fillMaxWidth(),
             enabled = navToDailyReport != null,
             isNewDailyReport = isNewDailyReport,
             onClick = navToDailyReport,
@@ -104,11 +110,12 @@ fun TimeCapsuleCalendarBottomSheet(
 private fun DailyReportButton(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    isNewDailyReport: Boolean,
+    isNewDailyReport: Boolean = false,
     onClick: (() -> Unit)? = null,
 ) {
     Box(
         modifier = modifier
+            .height((66.33).dp)
             .mainBackground(enabled, RoundedCornerShape(15.dp), MooiTheme.colorScheme.gray700)
             .clickable(enabled = enabled, onClick = { onClick?.invoke() }),
     ) {
@@ -118,6 +125,51 @@ private fun DailyReportButton(
             style = MooiTheme.typography.button,
             color = if (enabled) Color.White else MooiTheme.colorScheme.gray500,
         )
+        if (isNewDailyReport) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .offset(x = 78.dp, y = -13.dp)
+                    .size(20.dp)
+                    .background(Color(0xFF1C1A22).copy(alpha = 0.5f), CircleShape),
+            ) {
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    text = "N",
+                    style = TextStyle(
+                        fontFamily = pretendard,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 10.sp,
+                    ),
+                    color = Color.White,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun DailyReportButtonPreview() {
+    MooiTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MooiTheme.colorScheme.background)
+                .padding(10.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            DailyReportButton(
+                modifier = Modifier.fillMaxWidth(),
+                enabled = true,
+                isNewDailyReport = true,
+                onClick = {},
+            )
+            DailyReportButton(
+                modifier = Modifier.fillMaxWidth(),
+                enabled = false,
+            )
+        }
     }
 }
 
