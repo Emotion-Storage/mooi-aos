d84ff8ee386eb5f24b85f3f8295f94ed8171448c
harry <harry7408@naver.com>
2025-09-27 18:14:12 +0900

    feat: make common SpeechBubble Composable

--- core/ui/src/main/java/com/emotionstorage/ui/component/SpeechBubble.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/component/SpeechBubble.kt
@@ -1,8 +1,7 @@
-
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
@@ -11,85 +10,142 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.emotionstorage.ui.theme.MooiTheme
 import com.emotionstorage.ui.util.dpToPixel
 
+
+enum class BubbleTail { TopCenter, BottomCenter, TopLeft, }
+
 @Composable
 fun SpeechBubble(
     text: String,
-    modifier: Modifier = Modifier.size(265.dp, 84.dp),
+    modifier: Modifier = Modifier,
+    tail: BubbleTail = BubbleTail.BottomCenter,
+    sizeParam: DpSize = DpSize(265.dp, 101.dp),
+    cornerRadius: Dp = 10.dp,
+    tailWidth: Dp = 14.dp,
+    tailHeight: Dp = 12.dp,
+    tailOffset: Dp = 14.dp,
+    bgBrush: Brush = MooiTheme.brushScheme.subButtonBackground,
+    textStyle: TextStyle = MooiTheme.typography.body4.copy(lineHeight = 20.sp),
+    textColor: Color = MooiTheme.colorScheme.gray300,
 ) {
     val context = LocalContext.current
 
-    val cornerRadius = dpToPixel(context, 10f)
-    val tailWidth = dpToPixel(context, 14f)
-    val tailHeight = dpToPixel(context, 12f)
-    val bgBrush = MooiTheme.brushScheme.commentBackground
+    val cornerRadiusPx = dpToPixel(context, cornerRadius.value)
+    val tailWidthPx = dpToPixel(context, tailWidth.value)
+    val tailHeightPx = dpToPixel(context, tailHeight.value)
+    val tailOffsetPx = dpToPixel(context, tailOffset.value)
+
+    val padding = when (tail) {
+        BubbleTail.TopCenter, BubbleTail.TopLeft -> PaddingValues(top = tailHeight)
+        BubbleTail.BottomCenter -> PaddingValues(bottom = tailHeight)
+    }
 
     Box(
-        modifier = modifier,
+        modifier = Modifier
+            .then(Modifier.size(sizeParam))
     ) {
         Canvas(
-            modifier =
-                Modifier
-                    .matchParentSize(),
+            modifier = Modifier.matchParentSize()
         ) {
-            val rectBottom = size.height - tailHeight
+            val w = size.width
+            val h = size.height
 
-            val path =
-                Path().apply {
-                    addRoundRect(
-                        RoundRect(
-                            left = 0f,
-                            top = 0f,
-                            right = size.width,
-                            bottom = rectBottom,
-                            cornerRadius = CornerRadius(cornerRadius, cornerRadius),
-                        ),
-                    )
-                    moveTo(size.width / 2 - tailWidth / 2, rectBottom)
-                    lineTo(size.width / 2, size.height)
-                    lineTo(size.width / 2 + tailWidth / 2, rectBottom)
-                    close()
+            val path = Path().apply {
+                when (tail) {
+                    BubbleTail.BottomCenter -> {
+                        val rectBottom = h - tailHeightPx
+                        addRoundRect(
+                            RoundRect(
+                                left = 0f,
+                                top = 0f,
+                                right = w,
+                                bottom = rectBottom,
+                                cornerRadius = CornerRadius(cornerRadiusPx, cornerRadiusPx)
+                            )
+                        )
+                        // 하단 가운데 말풍선 꼬리
+                        moveTo(w / 2 - tailWidthPx / 2, rectBottom)
+                        lineTo(w / 2, h)
+                        lineTo(w / 2 + tailWidthPx / 2, rectBottom)
+                        close()
+                    }
+
+                    BubbleTail.TopCenter -> {
+                        val rectTop = tailHeightPx
+                        addRoundRect(
+                            RoundRect(
+                                left = 0f,
+                                top = rectTop,
+                                right = w,
+                                bottom = h,
+                                cornerRadius = CornerRadius(cornerRadiusPx, cornerRadiusPx)
+                            )
+                        )
+                        // 상단 가운데 말풍선 꼬리
+                        moveTo(w / 2 - tailWidthPx / 2, rectTop)
+                        lineTo(w / 2, 0f)
+                        lineTo(w / 2 + tailWidthPx / 2, rectTop)
+                        close()
+                    }
+
+                    BubbleTail.TopLeft -> {
+                        val rectTop = tailHeightPx
+                        addRoundRect(
+                            RoundRect(
+                                left = 0f,
+                                top = rectTop,
+                                right = w,
+                                bottom = h,
+                                cornerRadius = CornerRadius(cornerRadiusPx, cornerRadiusPx)
+                            )
+                        )
+                        // 좌상단 말풍선 꼬리
+                        moveTo(tailOffsetPx - tailWidthPx / 2, rectTop)
+                        lineTo(tailOffsetPx, 0f)
+                        lineTo(tailOffsetPx + tailWidthPx / 2, rectTop)
+                        close()
+                    }
                 }
+            }
+            drawPath(path = path, brush = bgBrush)
+        }
 
-            drawPath(
-                path = path,
-                brush = bgBrush,
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = text,
+                style = textStyle,
+                color = textColor,
+                textAlign = TextAlign.Center
             )
         }
-        Text(
-            modifier =
-                Modifier
-                    .align(Alignment.TopCenter)
-                    .offset(y = 15.dp),
-            text = text,
-            style = MooiTheme.typography.body4.copy(lineHeight = 20.sp),
-            color = MooiTheme.colorScheme.gray300,
-            textAlign = TextAlign.Center,
-        )
     }
 }
 
+
 @Preview
 @Composable
-private fun SpeechBubblePreview() {
+fun SpeechBubblePreview() {
     MooiTheme {
-        Box(
-            modifier =
-                Modifier
-                    .padding(50.dp)
-                    .background(MooiTheme.colorScheme.background),
-        ) {
-            SpeechBubble(
-                text = "비밀은 지켜드릴게요,\n당신의 감정을 편하게 나누어보세요.",
-            )
-        }
+        SpeechBubble(
+            text = "비밀은 지켜드릴게요,\n당신의 감정을 편하게 나누어보세요."
+        )
     }
 }
