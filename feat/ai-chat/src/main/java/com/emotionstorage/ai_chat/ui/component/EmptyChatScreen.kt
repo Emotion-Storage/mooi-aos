2a8c80073dcfbd878999c5ed43ce889afdc63fd3
harry <harry7408@naver.com>
2025-09-30 14:25:59 +0900

    feat: empty chatting screen

---
+++ feat/ai-chat/src/main/java/com/emotionstorage/ai_chat/ui/component/EmptyChatScreen.kt
@@ -0,0 +1,55 @@
+package com.emotionstorage.ai_chat.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.emotionstorage.ui.theme.MooiTheme
+
+@Composable
+fun EmptyChatScreen(
+    modifier: Modifier = Modifier,
+    onFocusInput: () -> Unit,
+) {
+    Box(modifier.fillMaxSize()) {
+        Column(
+            Modifier
+                .align(Alignment.Center)
+                .offset(0.dp, (-100).dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.size(100.dp),
+                painter = painterResource(com.emotionstorage.ui.R.drawable.emotion_0),
+                contentDescription = null
+            )
+            Spacer(modifier.padding(top = 18.dp))
+            Text(
+                text = "지금 나누고 싶은\n이야기가 있나요?",
+                style = MooiTheme.typography.head1
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun EmptyChatScreenPreview() {
+    MooiTheme {
+        EmptyChatScreen(
+            onFocusInput = {}
+        )
+    }
+}
