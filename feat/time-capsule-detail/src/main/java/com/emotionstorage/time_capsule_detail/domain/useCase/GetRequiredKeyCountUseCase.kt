c9272cb92c877e19a937eec64817b23579c3a418
sunjoolee <sheisthepoem@naver.com>
2025-10-01 14:02:22 +0900

    fix: fix required key count calc logic

--- feat/time-capsule-detail/src/main/java/com/emotionstorage/time_capsule_detail/domain/useCase/GetRequiredKeyCountUseCase.kt
+++ feat/time-capsule-detail/src/main/java/com/emotionstorage/time_capsule_detail/domain/useCase/GetRequiredKeyCountUseCase.kt
@@ -15,8 +15,8 @@ class GetRequiredKeyCountUseCase @Inject constructor() {
                 in 0..7 -> emit(DataState.Success(1))
                 in 8..30 -> emit(DataState.Success(3))
                 in 31..90 -> emit(DataState.Success(7))
-                in 90..180 -> emit(DataState.Success(11))
-                in 180..365 -> emit(DataState.Success(15))
+                in 91..180 -> emit(DataState.Success(11))
+                in 181..365 -> emit(DataState.Success(15))
                 else -> emit(DataState.Error(throwable = Throwable("invalid arriveAt")))
             }
         }
