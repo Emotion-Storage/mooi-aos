cfdb362dc088bbe9210b33def93b811ff589cdce
sunjoolee <sheisthepoem@naver.com>
2025-10-01 11:50:02 +0900

    feat: wrap return value with flow & data state

--- feat/time-capsule-detail/src/main/java/com/emotionstorage/time_capsule_detail/domain/useCase/getRequiredKeyCountUseCase.kt
+++ feat/time-capsule-detail/src/main/java/com/emotionstorage/time_capsule_detail/domain/useCase/getRequiredKeyCountUseCase.kt
@@ -1,18 +1,22 @@
 package com.emotionstorage.time_capsule_detail.domain.useCase
 
 import com.emotionstorage.common.getDaysBetween
+import com.emotionstorage.domain.common.DataState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import java.time.LocalDate
 import javax.inject.Inject
 import kotlin.math.absoluteValue
 
 class getRequiredKeyCountUseCase @Inject constructor() {
-    operator fun invoke(arriveAt: LocalDate): Int =
+    operator fun invoke(arriveAt: LocalDate): Flow<DataState<Int>> = flow {
         when (LocalDate.now().getDaysBetween(arriveAt).absoluteValue) {
-            in 0..7 -> 1
-            in 8..30 -> 3
-            in 31..90 -> 7
-            in 90..180 -> 11
-            in 180..365 -> 15
-            else -> throw Throwable("invalid arriveAt")
+            in 0..7 -> emit(DataState.Success(1))
+            in 8..30 -> emit(DataState.Success(3))
+            in 31..90 -> emit(DataState.Success(7))
+            in 90..180 -> emit(DataState.Success(11))
+            in 180..365 -> emit(DataState.Success(15))
+            else -> emit(DataState.Error(throwable = Throwable("invalid arriveAt")))
         }
+    }
 }
