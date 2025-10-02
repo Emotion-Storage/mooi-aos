736905fae86567bf83b0fdf8ad981ca6c5b3373d
sunjoolee <sheisthepoem@naver.com>
2025-10-02 10:26:58 +0900

    style: format with ktlint

--- feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/ui/CalendarScreen.kt
+++ feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/ui/CalendarScreen.kt
@@ -33,7 +33,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
