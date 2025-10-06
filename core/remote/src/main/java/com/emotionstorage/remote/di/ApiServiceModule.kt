3c25c4d23c810e0869f7e11436ee6453fc59f3da
sunjoolee <sheisthepoem@naver.com>
2025-10-06 20:07:23 +0900

    feat: time capsule api service di

---
+++ core/remote/src/main/java/com/emotionstorage/remote/di/ApiServiceModule.kt
@@ -0,0 +1,17 @@
+package com.emotionstorage.remote.di
+
+import com.emotionstorage.remote.api.TimeCapsuleApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiServiceModule {
+    @Singleton
+    @Provides
+    fun provideTimeCapsuleApiService(retrofit: Retrofit): TimeCapsuleApiService = retrofit.create(TimeCapsuleApiService::class.java)
+}
