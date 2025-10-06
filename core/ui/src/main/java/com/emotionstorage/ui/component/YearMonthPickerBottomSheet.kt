a53652c4a1885403b58d773b7bc4d06149d69edd
sunjoolee <sheisthepoem@naver.com>
2025-10-06 10:46:30 +0900

    chore: rename bottom sheet

--- core/ui/src/main/java/com/emotionstorage/ui/component/YearMonthPickerBottomSheet.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/component/YearMonthPickerBottomSheet.kt
@@ -1,21 +1,32 @@
 package com.emotionstorage.ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.emotionstorage.ui.theme.MooiTheme
 import com.orhanobut.logger.Logger
+import java.time.LocalDate
 import java.time.YearMonth
 
 private val CALENDER_MIN_YEAR_MONTH = YearMonth.of(1970, 1)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CalendarYearMonthBottomSheet(
+fun YearMonthPickerBottomSheet(
     modifier: Modifier = Modifier,
+    sheetState: SheetState = rememberModalBottomSheetState(),
     onDismissRequest: () -> Unit = {},
     selectedYearMonth: YearMonth = YearMonth.now(),
     onYearMonthSelect: (YearMonth) -> Unit = {},
@@ -29,6 +40,7 @@ fun CalendarYearMonthBottomSheet(
 
     BottomSheet(
         modifier = modifier,
+        sheetState = sheetState,
         onDismissRequest = onDismissRequest,
         confirmLabel = "확인",
         onConfirm = {
@@ -49,3 +61,28 @@ fun CalendarYearMonthBottomSheet(
         )
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+private fun YearMonthPickerBottomSheetPreview() {
+    MooiTheme {
+        MooiTheme {
+            Box(
+                modifier =
+                    Modifier
+                        .fillMaxSize()
+                        .background(MooiTheme.colorScheme.background),
+            ) {
+                YearMonthPickerBottomSheet(
+                    // open sheet state for preview
+                    sheetState =
+                        rememberStandardBottomSheetState(
+                            initialValue = SheetValue.Expanded,
+                        ),
+                )
+            }
+        }
+    }
+}
+
