a8e969b6054977025d551af3ee556b2aaced2dc8
sunjoolee <sheisthepoem@naver.com>
2025-10-06 23:03:05 +0900

    chore: delete stub use case logic

--- domain/src/main/java/com/emotionstorage/domain/useCase/timeCapsule/GetFavoriteTimeCapsulesUseCase.kt
+++ domain/src/main/java/com/emotionstorage/domain/useCase/timeCapsule/GetFavoriteTimeCapsulesUseCase.kt
@@ -1,12 +1,7 @@
 package com.emotionstorage.domain.useCase.timeCapsule
 
-import com.emotionstorage.domain.common.DataState
-import com.emotionstorage.domain.model.TimeCapsule
 import com.emotionstorage.domain.repo.FavoriteSortBy
 import com.emotionstorage.domain.repo.TimeCapsuleRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import java.time.LocalDateTime
 import javax.inject.Inject
 
 class GetFavoriteTimeCapsulesUseCase @Inject constructor(
@@ -14,47 +9,4 @@ class GetFavoriteTimeCapsulesUseCase @Inject constructor(
 ) {
     suspend operator fun invoke(sortBy: FavoriteSortBy) =
         timeCapsuleRepository.getFavoriteTimeCapsules(sortBy)
-
-//    suspend operator fun invoke(sortBy: FavoriteSortBy): Flow<DataState<List<TimeCapsule>>> =
-//        flow {
-//            // stub usecase
-//            val timeCapsules =
-//                (1..15)
-//                    .toList()
-//                    .map { it ->
-//                        TimeCapsule(
-//                            id = it.toString(),
-//                            status = TimeCapsule.STATUS.OPENED,
-//                            title = "오늘 아침에 친구를 만났는데, 친구가 늦었어..",
-//                            summary = "",
-//                            emotions =
-//                                listOf(
-//                                    TimeCapsule.Emotion(
-//                                        emoji = "\uD83D\uDE14",
-//                                        label = "서운함",
-//                                        percentage = 30.0f,
-//                                    ),
-//                                    TimeCapsule.Emotion(
-//                                        emoji = "\uD83D\uDE0A",
-//                                        label = "고마움",
-//                                        percentage = 30.0f,
-//                                    ),
-//                                    TimeCapsule.Emotion(
-//                                        emoji = "\uD83E\uDD70",
-//                                        label = "안정감",
-//                                        percentage = 80.0f,
-//                                    ),
-//                                ),
-//                            isFavorite = true,
-//                            favoriteAt = LocalDateTime.now(),
-//                            createdAt = LocalDateTime.now(),
-//                        )
-//                    }.sortedByDescending {
-//                        when (sortBy) {
-//                            FavoriteSortBy.FAVORITE_AT -> it.favoriteAt
-//                            FavoriteSortBy.NEWEST -> it.createdAt
-//                        }
-//                    }
-//            emit(DataState.Success(timeCapsules))
-//        }
 }
