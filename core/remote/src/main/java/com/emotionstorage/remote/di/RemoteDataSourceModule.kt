cfe68f9522809fd6d0bbb770da84491dde18bf01
harry <harry7408@naver.com>
2025-10-07 11:19:47 +0900

    fix: missing binding and package name change

--- core/remote/src/main/java/com/emotionstorage/remote/di/RemoteDataSourceModule.kt
+++ core/remote/src/main/java/com/emotionstorage/remote/di/RemoteDataSourceModule.kt
@@ -1,8 +1,10 @@
 package com.emotionstorage.remote.di
 
+import com.emotionstorage.data.dataSource.MyPageRemoteDataSource
 import com.emotionstorage.data.dataSource.TimeCapsuleRemoteDataSource
-import com.emotionstorage.remote.dataSourceImpl.TimeCapsuleRemoteDataSourceImpl
+import com.emotionstorage.remote.datasourceImpl.TimeCapsuleRemoteDataSourceImpl
 import com.emotionstorage.data.dataSource.UserRemoteDataSource
+import com.emotionstorage.remote.datasourceImpl.MyPageRemoteDataSourceImpl
 import com.emotionstorage.remote.datasourceImpl.UserRemoteDataSourceImpl
 import dagger.Binds
 import dagger.Module
@@ -16,5 +18,13 @@ abstract class RemoteDataSourceModule {
     @Binds
     @Singleton
     abstract fun bindTimeCapsuleRemoteDataSource(impl: TimeCapsuleRemoteDataSourceImpl): TimeCapsuleRemoteDataSource
-    abstract fun bindUserRemoteDataSource(userRemoteDataSourceImpl: UserRemoteDataSourceImpl): UserRemoteDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindUserRemoteDataSource(impl: UserRemoteDataSourceImpl): UserRemoteDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindMyPageRemoteDataSource(impl: MyPageRemoteDataSourceImpl): MyPageRemoteDataSource
+
 }
