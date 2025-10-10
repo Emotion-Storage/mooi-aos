9292c1319163ee55886021f13290a120a4b7db57
sunjoolee <sheisthepoem@naver.com>
2025-10-10 12:50:28 +0900

    feat: add timecapsule saved modal

---
+++ feat/time-capsule-detail/src/main/java/com/emotionstorage/time_capsule_detail/ui/modal/TimeCapsuleSavedModal.kt
@@ -0,0 +1,38 @@
+package com.emotionstorage.time_capsule_detail.ui.modal
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.emotionstorage.ui.component.Modal
+
+@Composable
+fun TimeCapsuleSavedModal(
+    isModalOpen: Boolean = false,
+    onConfirm: () -> Unit = {}
+) {
+    // todo: confirm modal automatically after 4 secs
+    if (isModalOpen) {
+        Modal(
+            title =
+                "타임캡슐을\n안전하게 보관했어요.",
+            bottomDescription = "이 감정은 추후 더\n소중한 이야기가 될 거예요.",
+            onDismissRequest = {},
+            confirmLabel = "네, 확인했어요.",
+            onConfirm = onConfirm,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TimeCapsuleSavedModalPreview() {
+    // background ui
+    Box(modifier = Modifier.fillMaxSize())
+
+    TimeCapsuleSavedModal(
+        isModalOpen = true,
+    )
+}
+
