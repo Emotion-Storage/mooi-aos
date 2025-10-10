15b72899d15b86d5aaf65b22a2168943daf27c94
sunjoolee <sheisthepoem@naver.com>
2025-10-11 06:51:03 +0900

    build: add daily-report module

---
+++ feat/daily-report/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.emotionstorage.convention.android.library")
+    id("com.emotionstorage.convention.android.library.compose")
+    id("com.emotionstorage.convention.android.library.hilt")
+}
+
+
+android {
+    namespace = "com.emotionstorage.daily_report"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(projects.domain)
+    implementation(projects.core.common)
+    implementation(projects.core.ui)
+}
