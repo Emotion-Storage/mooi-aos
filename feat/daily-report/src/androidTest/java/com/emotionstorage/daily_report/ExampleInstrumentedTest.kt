785301e191d8e9391ff21719f3204c9b1ce7571d
Sunjoo Lee <86330886+sunjoolee@users.noreply.github.com>
2025-10-11 11:18:22 +0900

    fix: fix text package name
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- feat/daily-report/src/androidTest/java/com/emotionstorage/daily_report/ExampleInstrumentedTest.kt
+++ feat/daily-report/src/androidTest/java/com/emotionstorage/daily_report/ExampleInstrumentedTest.kt
@@ -18,6 +18,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.emotionstorage.mylibrary.test", appContext.packageName)
+        assertEquals("com.emotionstorage.daily_report.test", appContext.packageName)
     }
 }
