6dbd6bdc57ad475ce5deac2dd3ad7d99ea70e754
sunjoolee <sheisthepoem@naver.com>
2025-10-11 10:47:57 +0900

    feat: daily report detail screen ui

--- feat/daily-report/src/main/java/com/emotionstorage/daily_report/ui/DailyReportDetailScreen.kt
+++ feat/daily-report/src/main/java/com/emotionstorage/daily_report/ui/DailyReportDetailScreen.kt
@@ -1,23 +1,30 @@
 package com.emotionstorage.daily_report.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.emotionstorage.common.toKorDate
 import com.emotionstorage.daily_report.presentation.DailyReportDetailAction
@@ -128,6 +135,39 @@ private fun StatelessDailyReportDetailScreen(
                     "스트레스 지수" to state.dailyReport!!.stressScore,
                     "행복 지수" to state.dailyReport!!.happinessScore,
                 )
+            )
+            Spacer(modifier = Modifier.height(53.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = Color(0xFF0E0C12).copy(alpha = 0.5f),
+                        shape = RoundedCornerShape(50),
+                    )
+                    .padding(vertical = 15.dp, horizontal = 38.dp),
+                verticalArrangement = Arrangement.spacedBy(2.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "감정 요약",
+                    style = MooiTheme.typography.body8.copy(lineHeight = 24.sp),
+                    color = MooiTheme.colorScheme.primary,
+                )
+                Text(
+                    text = state.dailyReport!!.emotionSummary,
+                    style = MooiTheme.typography.body8.copy(lineHeight = 24.sp),
+                    color = Color.White,
+                    textAlign = TextAlign.Center
+                )
+            }
+            Spacer(modifier = Modifier.height(42.dp))
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "하루의 대화를 바탕으로 작성된 일일리포트는\n" +
+                    "당일 마지막 감정 대화 기준 24시간 후 업데이트 됩니다.",
+                style = MooiTheme.typography.caption7.copy(fontWeight = FontWeight.Normal, lineHeight = 22.sp),
+                color = MooiTheme.colorScheme.gray400,
+                textAlign = TextAlign.Center
             )
         }
     }
