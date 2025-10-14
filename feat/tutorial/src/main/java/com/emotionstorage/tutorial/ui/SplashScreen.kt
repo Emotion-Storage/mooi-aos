dbb9ce97716928fc52bd3c326a787b7187fa189c
sunjoolee <sheisthepoem@naver.com>
2025-10-14 13:26:35 +0900

    feat: add splash graphic

--- feat/tutorial/src/main/java/com/emotionstorage/tutorial/ui/SplashScreen.kt
+++ feat/tutorial/src/main/java/com/emotionstorage/tutorial/ui/SplashScreen.kt
@@ -1,25 +1,31 @@
 package com.emotionstorage.tutorial.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.emotionstorage.tutorial.presentation.SplashAction
 import com.emotionstorage.tutorial.presentation.SplashSideEffect
 import com.emotionstorage.tutorial.presentation.SplashViewModel
+import com.emotionstorage.ui.R
 import com.emotionstorage.ui.theme.MooiTheme
 
 @Composable
@@ -48,24 +54,22 @@ private fun StatelessSplashScreen(modifier: Modifier = Modifier) {
     Scaffold(
         modifier =
             modifier
-                .fillMaxSize()
-                .background(MooiTheme.colorScheme.background),
+                .fillMaxSize(),
     ) { padding ->
-        Column(
+        Box(
             modifier =
                 Modifier
                     .fillMaxSize()
                     .background(MooiTheme.colorScheme.background)
                     .padding(padding),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Box(
-                modifier =
-                    Modifier
-                        .size(148.dp)
-                        .offset(y = 214.dp)
-                        .background(Color.Black),
+            Image(
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .padding(top = 215.dp)
+                    .width(235.dp),
+                painter = painterResource(R.drawable.splash_graphic),
+                contentDescription = "Mooi"
             )
         }
     }
@@ -75,6 +79,6 @@ private fun StatelessSplashScreen(modifier: Modifier = Modifier) {
 @Composable
 private fun SplashScreenPreview() {
     MooiTheme {
-        StatelessSplashScreen(modifier = Modifier.fillMaxSize())
+        StatelessSplashScreen()
     }
 }
