bd05c5a5b3bc92e80a2cfb3272fde9d30c3c4f34
sunjoolee <sheisthepoem@naver.com>
2025-10-14 16:45:33 +0900

    fix: fix arrived time capsule screen design
    fix paddings
    add favorite icon
    make entire screen scrollable

--- feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/ui/ArrivedTimeCapsulesScreen.kt
+++ feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/ui/ArrivedTimeCapsulesScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -72,6 +74,8 @@ fun ArrivedTimeCapsulesScreen(
     navToTimeCapsuleDetail: (id: String) -> Unit = {},
     navToBack: () -> Unit = {},
 ) {
+    // todo: add favorite toggle functionality?
+
     StatelessArrivedTimeCapsulesScreen(
         modifier = modifier,
         timeCapsules = DUMMY_TIME_CAPSULES,
@@ -87,8 +91,6 @@ private fun StatelessArrivedTimeCapsulesScreen(
     navToTimeCapsuleDetail: (id: String) -> Unit = {},
     navToBack: () -> Unit = {},
 ) {
-    val scrollState = rememberScrollState()
-
     Scaffold(
         modifier =
             modifier
@@ -98,27 +100,15 @@ private fun StatelessArrivedTimeCapsulesScreen(
             TopAppBar(title = "도착한 타임캡슐", showBackButton = true, onBackClick = navToBack)
         },
     ) { innerPadding ->
-        Box(
+        LazyColumn(
             modifier =
                 Modifier
                     .fillMaxSize()
                     .background(MooiTheme.colorScheme.background)
                     .padding(innerPadding)
-                    .padding(horizontal = 16.dp),
+                    .padding(horizontal = 16.dp)
         ) {
-            if (timeCapsules.isEmpty()) {
-                Text(
-                    modifier = Modifier.align(Alignment.Center),
-                    text = "최근 도착한 타임캡슐이 없어요.",
-                    style = MooiTheme.typography.caption2,
-                    color = MooiTheme.colorScheme.gray400,
-                )
-            }
-
-            Column(
-                modifier = Modifier.align(Alignment.TopCenter).fillMaxWidth(),
-            ) {
-                // info text
+            item {
                 Row(
                     modifier =
                         Modifier
@@ -133,7 +123,7 @@ private fun StatelessArrivedTimeCapsulesScreen(
                                 .width(12.dp)
                                 .height(14.dp),
                         contentDescription = "arrived",
-                        colorFilter = ColorFilter.tint(MooiTheme.colorScheme.gray500),
+                        colorFilter = ColorFilter.tint(MooiTheme.colorScheme.gray600),
                     )
                     Text(
                         text = "최근 3주간 도착한 타임캡슐을 표시합니다.\n도착한 타임캡슐을 열어 내 지난 감정을 확인해보세요.",
@@ -141,27 +131,32 @@ private fun StatelessArrivedTimeCapsulesScreen(
                         color = MooiTheme.colorScheme.gray500,
                     )
                 }
+            }
 
-                LazyColumn(
+            if (timeCapsules.isEmpty()) {
+                item {
+                    Text(
+                        modifier = Modifier.padding(top = 244.dp),
+                        text = "최근 도착한 타임캡슐이 없어요.",
+                        style = MooiTheme.typography.caption2,
+                        color = MooiTheme.colorScheme.gray400,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+
+            items(items = timeCapsules, key = { it.id }) {
+                TimeCapsuleItem(
                     modifier =
                         Modifier
                             .fillMaxWidth()
-                            .scrollable(scrollState, orientation = Orientation.Vertical),
-                    verticalArrangement = Arrangement.spacedBy(32.dp),
-                ) {
-                    items(items = timeCapsules, key = { it.id }) {
-                        TimeCapsuleItem(
-                            modifier =
-                                Modifier
-                                    .fillMaxWidth(),
-                            timeCapsule = it,
-                            showDate = true,
-                            showInfoText = false,
-                            showFavorite = true,
-                            onClick = { navToTimeCapsuleDetail(it.id) },
-                        )
-                    }
-                }
+                            .padding(bottom = 26.dp),
+                    timeCapsule = it,
+                    showDate = true,
+                    showInfoText = false,
+                    showFavorite = true,
+                    onClick = { navToTimeCapsuleDetail(it.id) },
+                )
             }
         }
     }
