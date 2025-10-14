fe53958ce8fce8ac9b8353b8a775dd849eaf4542
sunjoolee <sheisthepoem@naver.com>
2025-10-14 13:42:38 +0900

    fix: fix login screen padding

--- feat/auth/src/main/java/com/emotionstorage/auth/ui/LoginScreen.kt
+++ feat/auth/src/main/java/com/emotionstorage/auth/ui/LoginScreen.kt
@@ -1,12 +1,15 @@
 package com.emotionstorage.auth.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -16,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
@@ -74,9 +78,11 @@ private fun StatelessLoginScreen(
     val scrollState = rememberScrollState()
 
     Scaffold(
-        modifier
-            .background(MooiTheme.colorScheme.background)
-            .fillMaxSize(),
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
+        modifier =
+            modifier
+                .background(MooiTheme.colorScheme.background)
+                .fillMaxSize(),
     ) { padding ->
         Column(
             modifier =
@@ -84,17 +90,26 @@ private fun StatelessLoginScreen(
                     .background(MooiTheme.colorScheme.background)
                     .fillMaxSize()
                     .padding(padding)
+                    .padding(horizontal = 16.dp)
                     .padding(top = 67.dp, bottom = 36.dp)
                     .verticalScroll(scrollState),
             verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(
-                modifier = Modifier.height(37.dp),
-                style = MooiTheme.typography.body2,
-                color = MooiTheme.colorScheme.primary,
-                text = stringResource(id = R.string.login_title),
-            )
+            Column(
+                modifier =
+                    Modifier
+                        .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    modifier = Modifier.height(37.dp),
+                    style = MooiTheme.typography.body2,
+                    color = MooiTheme.colorScheme.primary,
+                    text = stringResource(id = R.string.login_title),
+                )
+            }
+
             Column(
                 modifier =
                     Modifier
--- feat/auth/src/main/java/com/emotionstorage/auth/ui/component/SocialLoginButton.kt
+++ feat/auth/src/main/java/com/emotionstorage/auth/ui/component/SocialLoginButton.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -34,7 +35,7 @@ fun SocialLoginButton(
     Button(
         modifier =
             modifier
-                .width(328.dp)
+                .fillMaxWidth()
                 .height(64.dp),
         shape = RoundedCornerShape(15.dp),
         elevation = ButtonDefaults.buttonElevation(0.dp, 0.dp, 0.dp, 0.dp, 0.dp),
