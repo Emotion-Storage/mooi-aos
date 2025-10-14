09599722070337c307d015d8d7d881eaa3ffa7c3
sunjoolee <sheisthepoem@naver.com>
2025-10-14 16:06:07 +0900

    feat: set text box input cursor color

--- core/ui/src/main/java/com/emotionstorage/ui/component/TextBoxInput.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/component/TextBoxInput.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.emotionstorage.ui.theme.MooiTheme
@@ -46,6 +47,7 @@ fun TextBoxInput(
             ),
         value = if (value.length > maxCharCount) value.substring(0, maxCharCount) else value,
         onValueChange = onValueChange,
+        cursorBrush = SolidColor(MooiTheme.colorScheme.primary)
     ) {
         Column(
             modifier =
