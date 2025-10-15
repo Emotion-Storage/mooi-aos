4361a21fc303e34868ec14d55d3090de42761546
harry <harry7408@naver.com>
2025-10-15 10:42:11 +0900

    feat: term Detail Screen

---
+++ feat/tutorial/src/main/java/com/emotionstorage/tutorial/ui/onBoarding/TermDetailScreen.kt
@@ -0,0 +1,65 @@
+package com.emotionstorage.tutorial.ui.onBoarding
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.emotionstorage.ui.component.TermsOfServiceContent
+import com.emotionstorage.ui.component.TopAppBar
+import com.emotionstorage.ui.theme.MooiTheme
+
+@Composable
+fun TermDetailScreen(
+    navToBack: () -> Unit = {},
+) {
+    Scaffold(
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
+        topBar = {
+            TopAppBar(
+                showBackground = false,
+                showBackButton = true,
+                onBackClick = navToBack,
+                handleBackPress = true,
+                onHandleBackPress = navToBack,
+            )
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .consumeWindowInsets(WindowInsets.navigationBars)
+                .background(MooiTheme.colorScheme.background),
+        ) {
+            Column {
+                Text(
+                    modifier = Modifier.padding(start = 16.dp, top = 24.dp),
+                    text = "MOOI 이용 약관",
+                    style = MooiTheme.typography.head1,
+                    color = Color.White,
+                )
+
+                TermsOfServiceContent()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TermDetailScreenPreview() {
+    MooiTheme {
+        TermDetailScreen()
+    }
+}
