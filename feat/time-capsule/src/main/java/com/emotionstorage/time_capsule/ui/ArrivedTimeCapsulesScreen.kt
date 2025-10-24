e44674c049e1ca7dbe872c181bb978fb23bae1c8
sunjoolee <sheisthepoem@naver.com>
2025-10-24 21:59:22 +0900

    feat: arrived time capsule screen wip

--- feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/ui/ArrivedTimeCapsulesScreen.kt
+++ feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/ui/ArrivedTimeCapsulesScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
@@ -23,7 +25,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.emotionstorage.domain.model.TimeCapsule
+import com.emotionstorage.time_capsule.presentation.ArrivedTimeCapsulesAction
+import com.emotionstorage.time_capsule.presentation.ArrivedTimeCapsulesSideEffect
+import com.emotionstorage.time_capsule.presentation.ArrivedTimeCapsulesViewModel
 import com.emotionstorage.time_capsule.ui.component.TimeCapsuleItem
 import com.emotionstorage.time_capsule.ui.model.TimeCapsuleItemState
 import com.emotionstorage.ui.R
@@ -31,49 +37,30 @@ import com.emotionstorage.ui.component.TopAppBar
 import com.emotionstorage.ui.theme.MooiTheme
 import java.time.LocalDateTime
 
-private val DUMMY_TIME_CAPSULES =
-    (1..15).toList().map { it ->
-        TimeCapsuleItemState(
-            id = it.toString(),
-            status = TimeCapsule.Status.ARRIVED,
-            title = "오늘 아침에 친구를 만났는데, 친구가 늦었어..",
-            emotions =
-                listOf(
-                    TimeCapsule.Emotion(
-                        emoji = "\uD83D\uDE14",
-                        label = "서운함",
-                        percentage = 30.0f,
-                    ),
-                    TimeCapsule.Emotion(
-                        emoji = "\uD83D\uDE0A",
-                        label = "고마움",
-                        percentage = 30.0f,
-                    ),
-                    TimeCapsule.Emotion(
-                        emoji = "\uD83E\uDD70",
-                        label = "안정감",
-                        percentage = 80.0f,
-                    ),
-                ),
-            isFavorite = true,
-            isFavoriteAt = LocalDateTime.now(),
-            createdAt = LocalDateTime.now(),
-            expireAt = LocalDateTime.now().plusHours(5),
-            openDDay = it,
-        )
-    }
-
 @Composable
 fun ArrivedTimeCapsulesScreen(
     modifier: Modifier = Modifier,
+    viewModel: ArrivedTimeCapsulesViewModel = hiltViewModel(),
     navToTimeCapsuleDetail: (id: String) -> Unit = {},
     navToBack: () -> Unit = {},
 ) {
-    // todo: add favorite toggle functionality?
+    val state = viewModel.container.stateFlow.collectAsState()
+
+    LaunchedEffect("init") {
+        viewModel.onAction(ArrivedTimeCapsulesAction.Init)
+
+        viewModel.container.sideEffectFlow.collect {
+            when(it){
+                is ArrivedTimeCapsulesSideEffect.ShowToast -> {
+                    // todo: show toast
+                }
+            }
+        }
+    }
 
     StatelessArrivedTimeCapsulesScreen(
         modifier = modifier,
-        timeCapsules = DUMMY_TIME_CAPSULES,
+        timeCapsules = state.value.timeCapsules,
         navToTimeCapsuleDetail = navToTimeCapsuleDetail,
         navToBack = navToBack,
     )
@@ -96,6 +83,8 @@ private fun StatelessArrivedTimeCapsulesScreen(
             TopAppBar(title = "도착한 타임캡슐", showBackButton = true, onBackClick = navToBack)
         },
     ) { innerPadding ->
+        // todo: pull down to refresh
+        // todo: load more on scroll end
         LazyColumn(
             modifier =
                 Modifier
@@ -164,7 +153,36 @@ private fun StatelessArrivedTimeCapsulesScreen(
 private fun ArrivedTimeCapsulesScreenPreview() {
     MooiTheme {
         StatelessArrivedTimeCapsulesScreen(
-            timeCapsules = DUMMY_TIME_CAPSULES,
+            timeCapsules = (1..15).toList().map { it ->
+                TimeCapsuleItemState(
+                    id = it.toString(),
+                    status = TimeCapsule.Status.ARRIVED,
+                    title = "오늘 아침에 친구를 만났는데, 친구가 늦었어..",
+                    emotions =
+                        listOf(
+                            TimeCapsule.Emotion(
+                                emoji = "\uD83D\uDE14",
+                                label = "서운함",
+                                percentage = 30.0f,
+                            ),
+                            TimeCapsule.Emotion(
+                                emoji = "\uD83D\uDE0A",
+                                label = "고마움",
+                                percentage = 30.0f,
+                            ),
+                            TimeCapsule.Emotion(
+                                emoji = "\uD83E\uDD70",
+                                label = "안정감",
+                                percentage = 80.0f,
+                            ),
+                        ),
+                    isFavorite = true,
+                    isFavoriteAt = LocalDateTime.now(),
+                    createdAt = LocalDateTime.now(),
+                    expireAt = LocalDateTime.now().plusHours(5),
+                    openDDay = it,
+                )
+            }
         )
     }
 }
