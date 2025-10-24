737aa61439118414b1eddc3cd1abb17afc81d360
harry <harry7408@naver.com>
2025-10-24 12:17:44 +0900

    feat: change chat textInput cursor Brush color

--- feat/ai-chat/src/main/java/com/emotionstorage/ai_chat/ui/component/ChatMessageInputBox.kt
+++ feat/ai-chat/src/main/java/com/emotionstorage/ai_chat/ui/component/ChatMessageInputBox.kt
@@ -78,6 +78,7 @@ fun ChatMessageInputBox(
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Send),
                 keyboardActions = KeyboardActions(onSend = { if (canSend) onSendMessage() }),
                 interactionSource = interaction,
+                cursorBrush = SolidColor(MooiTheme.colorScheme.primary),
                 decorationBox = { inner ->
                     Row(
                         modifier =
