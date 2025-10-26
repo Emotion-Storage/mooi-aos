d5f7d39ca570cf68b7daa426769fcd344a794cef
sunjoolee <sheisthepoem@naver.com>
2025-10-26 19:48:28 +0900

    feat: add get daily report api

--- core/remote/src/main/java/com/emotionstorage/remote/di/RetrofitModule.kt
+++ core/remote/src/main/java/com/emotionstorage/remote/di/RetrofitModule.kt
@@ -2,7 +2,9 @@ package com.emotionstorage.remote.di
 
 import com.emotionstorage.data.dataSource.SessionLocalDataSource
 import com.emotionstorage.remote.BuildConfig
+import com.emotionstorage.remote.api.DailyReportApiService
 import com.emotionstorage.remote.api.MyPageApiService
+import com.emotionstorage.remote.api.TimeCapsuleApiService
 import com.emotionstorage.remote.api.UserApiService
 import com.emotionstorage.remote.interceptor.RequestHeaderInterceptor
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
@@ -63,11 +65,22 @@ object RetrofitModule {
     fun provideRequestHeaderInterceptor(sessionLocalDataSource: SessionLocalDataSource) =
         RequestHeaderInterceptor(sessionLocalDataSource)
 
-    @Provides
+
     @Singleton
-    fun bindUserApiService(retrofit: Retrofit): UserApiService = retrofit.create(UserApiService::class.java)
+    @Provides
+    fun provideTimeCapsuleApiService(retrofit: Retrofit): TimeCapsuleApiService =
+        retrofit.create(TimeCapsuleApiService::class.java)
+
+    @Singleton
+    @Provides
+    fun provideDailyReportApiService(retrofit: Retrofit): DailyReportApiService =
+        retrofit.create(DailyReportApiService::class.java)
 
     @Provides
     @Singleton
-    fun bindMyPageApiService(retrofit: Retrofit): MyPageApiService = retrofit.create(MyPageApiService::class.java)
+    fun provideUserApiService(retrofit: Retrofit): UserApiService = retrofit.create(UserApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideMyPageApiService(retrofit: Retrofit): MyPageApiService = retrofit.create(MyPageApiService::class.java)
 }
---
+++ core/remote/src/main/java/com/emotionstorage/remote/response/dailyReport/GetDailyReportResponse.kt
@@ -0,0 +1,29 @@
+package com.emotionstorage.remote.response.dailyReport
+
+import com.emotionstorage.common.LocalDateTimeSerializer
+import kotlinx.serialization.Serializable
+import java.time.LocalDateTime
+
+@Serializable
+data class GetDailyReportResponse(
+    val id: Long,
+    val isOpen: Boolean,
+    @Serializable(with = LocalDateTimeSerializer::class)
+    val createdAt: LocalDateTime,
+    @Serializable(with = LocalDateTimeSerializer::class)
+    val updatedAt: LocalDateTime,
+    val summaries: List<String>,
+    val keywords: List<String>,
+    val stressIndex: Int,
+    val happinessIndex: Int,
+    val emotionSummary: String,
+    val emotionChanges: List<EmotionChange> = emptyList(),
+) {
+    @Serializable
+    data class EmotionChange(
+        // HH:mm
+        val time: String,
+        val label: String,
+        val description: String,
+    )
+}
