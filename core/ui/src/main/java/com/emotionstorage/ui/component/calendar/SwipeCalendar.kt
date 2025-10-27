d8d99d56385e8fb2a515d178afb63ae6a9d66245
sunjoolee <sheisthepoem@naver.com>
2025-10-27 23:21:06 +0900

    feat: swiper calendar component wip

---
+++ core/ui/src/main/java/com/emotionstorage/ui/component/calendar/SwipeCalendar.kt
@@ -0,0 +1,77 @@
+package com.emotionstorage.ui.component.calendar
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.emotionstorage.ui.theme.MooiTheme
+import java.time.LocalDate
+import java.time.YearMonth
+
+@Composable
+fun SwipeCalendar(
+    minYearMonth: YearMonth,
+    maxYearMonth: YearMonth,
+    modifier: Modifier = Modifier,
+    calendarYearMonth: YearMonth = YearMonth.now(),
+    onCalendarYearMonthSelect: (yearMonth: YearMonth) -> Unit = {},
+    showYearMonthDropDownIcon: Boolean = false,
+    onYearMonthDropDownIconClick: () -> Unit = {},
+    calendarYearMonthTextStyle: TextStyle = MooiTheme.typography.mainButton,
+    weekDateItem: @Composable (modifier: Modifier, label: String) -> Unit = { modifier, label ->
+        Text(
+            modifier = modifier.padding(vertical = 16.dp),
+            text = label,
+            style = MooiTheme.typography.caption5,
+            color = MooiTheme.colorScheme.gray400,
+            textAlign = TextAlign.Center,
+        )
+
+    },
+    dateItem: @Composable (modifier: Modifier, date: LocalDate) -> Unit = { modifier, date ->
+        Text(
+            modifier = modifier.padding(bottom = 6.dp),
+            text = date.dayOfMonth.toString(),
+            style = MooiTheme.typography.caption5,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+        )
+    }
+) {
+    Column(modifier = modifier){
+        CalendarYearMonthIndicator(
+            calendarYearMonth = calendarYearMonth,
+            minYearMonth = minYearMonth,
+            maxYearMonth = maxYearMonth,
+            onCalendarYearMonthSelect = onCalendarYearMonthSelect,
+            showYearMonthDropDownIcon = showYearMonthDropDownIcon,
+            onYearMonthDropDownIconClick = onYearMonthDropDownIconClick,
+            calendarYearMonthTextStyle = calendarYearMonthTextStyle,
+        )
+
+        // add horizontal pager
+    }
+}
+
+@Preview
+@Composable
+private fun SwipeCalendarPreview() {
+    val (calendarYearMonth, setCalendarYearMonth) = remember { mutableStateOf(YearMonth.now()) }
+
+    MooiTheme {
+        SwipeCalendar(
+            calendarYearMonth = calendarYearMonth,
+            minYearMonth = YearMonth.now().minusYears(1),
+            maxYearMonth = YearMonth.now().plusYears(1),
+            onCalendarYearMonthSelect = setCalendarYearMonth,
+        )
+    }
+}
