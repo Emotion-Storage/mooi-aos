ab02748ac3093fa8984f91f2c3e7f62afa6f081a
sunjoolee <sheisthepoem@naver.com>
2025-10-28 13:13:56 +0900

    style: format code

--- core/ui/src/main/java/com/emotionstorage/ui/component/calendar/CalendarDates.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/component/calendar/CalendarDates.kt
@@ -1,8 +1,6 @@
 package com.emotionstorage.ui.component.calendar
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -10,8 +8,6 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -34,7 +30,7 @@ fun CalendarDates(
             color = Color.White,
             textAlign = TextAlign.Center,
         )
-    }
+    },
 ) {
     // calendar dates
     LazyVerticalGrid(
@@ -50,7 +46,6 @@ fun CalendarDates(
     }
 }
 
-
 @Preview
 @Composable
 private fun CalendarDatesPreview() {
@@ -61,4 +56,3 @@ private fun CalendarDatesPreview() {
         )
     }
 }
-
