981dea814484544699bf818e672b39123b2347f4
sunjoolee <sheisthepoem@naver.com>
2025-11-05 22:36:05 +0900

    chore: rename usecase

--- domain/src/main/java/com/emotionstorage/domain/useCase/auth/GetAccessTokenUseCase.kt
+++ domain/src/main/java/com/emotionstorage/domain/useCase/auth/GetAccessTokenUseCase.kt
@@ -3,7 +3,7 @@ package com.emotionstorage.domain.useCase.auth
 import com.emotionstorage.domain.repo.SessionRepository
 import javax.inject.Inject
 
-class GetTokenUseCase @Inject constructor(
+class GetAccessTokenUseCase @Inject constructor(
     private val sessionRepository: SessionRepository,
 ) {
     suspend operator fun invoke(): String? = sessionRepository.getSession()?.accessToken
