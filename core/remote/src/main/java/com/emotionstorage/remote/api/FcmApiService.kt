e7da03a6386de83a5e6c53d65f66a72fad3f42c3
sunjoolee <sheisthepoem@naver.com>
2025-11-06 13:40:58 +0900

    fix: send token via header on delete request

--- core/remote/src/main/java/com/emotionstorage/remote/api/FcmApiService.kt
+++ core/remote/src/main/java/com/emotionstorage/remote/api/FcmApiService.kt
@@ -1,14 +1,20 @@
 package com.emotionstorage.remote.api
 
-import com.emotionstorage.remote.request.fcm.FcmTokenRequest
+import com.emotionstorage.remote.request.fcm.PostFcmTokenRequest
 import com.emotionstorage.remote.response.ResponseDto
+import retrofit2.http.Body
 import retrofit2.http.DELETE
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface FcmApiService {
     @POST("api/v1/users/fcm-token")
-    suspend fun postFcmToken(body: FcmTokenRequest): ResponseDto<Unit>
+    suspend fun postFcmToken(
+        @Body request: PostFcmTokenRequest
+    ): ResponseDto<Unit>
 
     @DELETE("api/v1/users/fcm-token")
-    suspend fun deleteFcmToken(body: FcmTokenRequest): ResponseDto<Unit>
+    suspend fun deleteFcmToken(
+        @Header("X-FCM-Token") token: String
+    ): ResponseDto<Unit>
 }
