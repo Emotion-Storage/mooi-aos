10df524ca46a35c4ed7419749ec14b5f3ac0a4fb
sunjoolee <sheisthepoem@naver.com>
2025-11-06 13:10:02 +0900

    fix: add android entry point annotation

--- app/src/main/java/com/emotionstorage/emotionstorage/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/emotionstorage/emotionstorage/MyFirebaseMessagingService.kt
@@ -9,11 +9,13 @@ import androidx.work.WorkerParameters
 import com.emotionstorage.domain.useCase.fcm.SaveOrRegisterFcmTokenUseCase
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     @Inject
     lateinit var saveOrRegisterFcmToken: SaveOrRegisterFcmTokenUseCase
