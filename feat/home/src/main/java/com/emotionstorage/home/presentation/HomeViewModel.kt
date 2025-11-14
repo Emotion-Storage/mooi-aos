827a5efe1d3627079931d97f3b1a7ae154634bba
sunjoolee <sheisthepoem@naver.com>
2025-11-14 13:44:18 +0900

    refactor: refactor home viewmodel

--- feat/home/src/main/java/com/emotionstorage/home/presentation/HomeViewModel.kt
+++ feat/home/src/main/java/com/emotionstorage/home/presentation/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.emotionstorage.home.presentation
 import androidx.lifecycle.ViewModel
 import com.emotionstorage.domain.useCase.chat.GetChatRoomIdUseCase
 import com.emotionstorage.domain.common.DataState
+import com.emotionstorage.domain.common.collectDataState
 import com.emotionstorage.domain.useCase.user.GetUserNicknameUseCase
 import com.emotionstorage.domain.useCase.home.GetHomeUseCase
 import com.orhanobut.logger.Logger
@@ -36,90 +37,80 @@ sealed class HomeSideEffect {
 @OptIn(OrbitExperimental::class)
 @HiltViewModel
 class HomeViewModel
-    @Inject
-    constructor(
-        private val getUserNickname: GetUserNicknameUseCase,
-        private val getHome: GetHomeUseCase,
-        private val getChatRoomId: GetChatRoomIdUseCase,
-    ) : ViewModel(),
-        ContainerHost<HomeState, HomeSideEffect> {
-        override val container = container<HomeState, HomeSideEffect>(HomeState())
+@Inject
+constructor(
+    private val getUserNickname: GetUserNicknameUseCase,
+    private val getHome: GetHomeUseCase,
+    private val getChatRoomId: GetChatRoomIdUseCase,
+) : ViewModel(),
+    ContainerHost<HomeState, HomeSideEffect> {
+    override val container = container<HomeState, HomeSideEffect>(HomeState())
 
-        fun onAction(action: HomeAction) {
-            when (action) {
-                is HomeAction.Initiate -> {
-                    handleInitiate()
-                }
+    fun onAction(action: HomeAction) {
+        when (action) {
+            is HomeAction.Initiate -> {
+                handleInitiate()
+            }
 
-                is HomeAction.EnterChat -> {
-                    handleEnterChat()
-                }
+            is HomeAction.EnterChat -> {
+                handleEnterChat()
             }
         }
-
-        private fun handleInitiate() =
-            intent {
-                initNickname()
-                initHomeState()
-            }
-
-        private suspend fun initHomeState() =
-            subIntent {
-                getHome().collect {
-                    when (it) {
-                        is DataState.Success -> {
-                            reduce {
-                                state.copy(
-                                    keyCount = it.data.keyCount,
-                                    ticketCount = it.data.ticketCount,
-                                    newNotificationArrived = it.data.hasNewNotification,
-                                    newTimeCapsuleArrived = it.data.hasNewTimeCapsule,
-                                    newReportArrived = it.data.hasNewReport,
-                                )
-                            }
-                        }
-
-                        is DataState.Error -> {
-                            Logger.e("HomeViewModel: handleUpdateState error: $it")
-                        }
-
-                        is DataState.Loading -> {
-                            // do nothing
-                        }
-                    }
-                }
-            }
-
-        private suspend fun initNickname() =
-            subIntent {
-                getUserNickname().collect {
-                    when (it) {
-                        is DataState.Success -> {
-                            reduce {
-                                state.copy(nickname = it.data)
-                            }
-                        }
-
-                        is DataState.Error -> {
-                            Logger.e("HomeViewModel: handleInitNickname error: $it")
-                        }
-
-                        is DataState.Loading -> {
-                            // do nothing
-                        }
-                    }
-                }
-            }
-
-        private fun handleEnterChat() =
-            intent {
-                getChatRoomId().collect {
-                    if (it is DataState.Success) {
-                        postSideEffect(HomeSideEffect.EnterCharRoomSuccess(it.data))
-                    }
-                    if (it is DataState.Error) {
-                        Logger.e("HomeViewModel: handleEnterChat error: $it")
-                    }
-                }
-            }
     }
+
+    private fun handleInitiate() =
+        intent {
+            initNickname()
+            initHomeState()
+        }
+
+    private suspend fun initHomeState() =
+        subIntent {
+            collectDataState(
+                flow = getHome(),
+                onSuccess = {
+                    reduce {
+                        state.copy(
+                            keyCount = it.keyCount,
+                            ticketCount = it.ticketCount,
+                            newNotificationArrived = it.hasNewNotification,
+                            newTimeCapsuleArrived = it.hasNewTimeCapsule,
+                            newReportArrived = it.hasNewReport,
+                        )
+                    }
+                },
+                onError = { throwable, _ ->
+                    Logger.e("HomeViewModel: handleUpdateState error: $throwable")
+                }
+            )
+        }
+
+    private suspend fun initNickname() =
+        subIntent {
+            collectDataState(
+                flow = getUserNickname(),
+                onSuccess = {
+                    reduce {
+                        state.copy(nickname = it)
+                    }
+                },
+                onError = { throwable, _ ->
+                    Logger.e("HomeViewModel: handleInitNickname error: $throwable")
+                }
+            )
+        }
+
+    private fun handleEnterChat() =
+        intent {
+            collectDataState(
+                flow = getChatRoomId(),
+                onSuccess = {
+                    postSideEffect(HomeSideEffect.EnterCharRoomSuccess(it))
+                },
+                onError = { throwable, _ ->
+                    Logger.e("HomeViewModel: handleEnterChat error: $throwable")
+                    // todo: add error popup
+                }
+            )
+        }
+}
