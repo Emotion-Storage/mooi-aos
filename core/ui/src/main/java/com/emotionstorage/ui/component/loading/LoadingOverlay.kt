f208cbfee763987279f7db76e445c3cdf262b8a2
sunjoolee <sheisthepoem@naver.com>
2025-11-18 11:03:36 +0900

    fix: fix overlay bg color

--- core/ui/src/main/java/com/emotionstorage/ui/component/loading/LoadingOverlay.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/component/loading/LoadingOverlay.kt
@@ -1,10 +1,12 @@
 package com.emotionstorage.ui.component.loading
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.zIndex
 import com.emotionstorage.ui.theme.MooiTheme
 
@@ -16,7 +18,7 @@ fun LoadingOverlay(
         modifier = modifier
             .fillMaxSize()
             .zIndex(20f)
-            .background(color = MooiTheme.colorScheme.background.copy(alpha = 0.7f)),
+            .background(color = Color.Black.copy(alpha = 0.7f)),
         contentAlignment = Alignment.Center
     ) {
         LoadingDots()
