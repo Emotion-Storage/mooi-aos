b0656874b7e6c2214aae23b84087bc8da9a34672
sunjoolee <sheisthepoem@naver.com>
2025-11-20 17:44:37 +0900

    feat: add modal top outer content

--- core/ui/src/main/java/com/emotionstorage/ui/component/Modal.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/component/Modal.kt
@@ -1,14 +1,18 @@
 package com.emotionstorage.ui.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -18,12 +22,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogWindowProvider
+import androidx.compose.ui.zIndex
+import com.emotionstorage.ui.R
 import com.emotionstorage.ui.component.button.CtaButton
 import com.emotionstorage.ui.component.button.CtaButtonType
 import com.emotionstorage.ui.theme.MooiTheme
@@ -39,91 +46,105 @@ fun Modal(
     dismissLabel: String? = null,
     onDismiss: () -> Unit = {},
     contentPadding: PaddingValues = PaddingValues(top = 22.dp, bottom = 28.dp, start = 30.dp, end = 30.dp),
+    showBackground: Boolean = true,
+    topOuterContent: @Composable (() -> Unit)? = null,
     content: @Composable (() -> Unit)? = null,
 ) {
-    // set bg color to black with 0.8f dim
-    Box(modifier = Modifier.fillMaxSize().background(Color.Black.copy(alpha = 0.8f)))
+    if (showBackground) {
+        // set bg color to black with 0.8f dim
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.8f))
+        )
+    }
+
     Dialog(onDismissRequest = onDismissRequest) {
         // set dim amount to 0.8f
         (LocalView.current.parent as DialogWindowProvider).window.setDimAmount(0.8f)
 
         Column(
-            modifier =
-                Modifier
-                    .clip(RoundedCornerShape(15.dp))
-                    .background(MooiTheme.colorScheme.background)
-                    .padding(contentPadding)
-                    .widthIn(max = 293.dp),
-            verticalArrangement = Arrangement.spacedBy(18.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            // title & descriptions
+            topOuterContent?.invoke()
             Column(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(6.dp),
+                modifier =
+                    Modifier
+                        .clip(RoundedCornerShape(15.dp))
+                        .background(MooiTheme.colorScheme.background)
+                        .padding(contentPadding)
+                        .widthIn(max = 293.dp),
+                verticalArrangement = Arrangement.spacedBy(18.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                if (!topDescription.isNullOrEmpty()) {
-                    Text(
-                        text = topDescription,
-                        style = MooiTheme.typography.body5,
-                        color = MooiTheme.colorScheme.gray500,
-                        textAlign = TextAlign.Center,
-                    )
+                // title & descriptions
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(6.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    if (!topDescription.isNullOrEmpty()) {
+                        Text(
+                            text = topDescription,
+                            style = MooiTheme.typography.body5,
+                            color = MooiTheme.colorScheme.gray500,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                    if (!title.isNullOrEmpty()) {
+                        Text(
+                            text = title,
+                            style =
+                                MooiTheme.typography.head2.copy(
+                                    lineHeight = 30.sp,
+                                ),
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                    if (!bottomDescription.isNullOrEmpty()) {
+                        Text(
+                            text = bottomDescription,
+                            style = MooiTheme.typography.body5,
+                            color = MooiTheme.colorScheme.gray500,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
                 }
-                if (!title.isNullOrEmpty()) {
-                    Text(
-                        text = title,
-                        style =
-                            MooiTheme.typography.head2.copy(
-                                lineHeight = 30.sp,
-                            ),
-                        color = Color.White,
-                        textAlign = TextAlign.Center,
-                    )
-                }
-                if (!bottomDescription.isNullOrEmpty()) {
-                    Text(
-                        text = bottomDescription,
-                        style = MooiTheme.typography.body5,
-                        color = MooiTheme.colorScheme.gray500,
-                        textAlign = TextAlign.Center,
-                    )
-                }
-            }
 
-            content?.invoke()
+                content?.invoke()
 
-            // buttons
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(12.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                if (confirmLabel != null) {
-                    CtaButton(
-                        modifier = Modifier.height(50.dp),
-                        labelString = confirmLabel,
-                        onClick = {
-                            onConfirm()
-                            onDismissRequest()
-                        },
-                        radius = 10,
-                        textStyle = MooiTheme.typography.mainButton,
-                    )
-                }
-                if (dismissLabel != null) {
-                    CtaButton(
-                        modifier = Modifier.height(50.dp),
-                        labelString = dismissLabel,
-                        onClick = {
-                            onDismiss()
-                            onDismissRequest()
-                        },
-                        type = CtaButtonType.TONAL,
-                        radius = 10,
-                        textStyle = MooiTheme.typography.mainButton,
-                    )
+                // buttons
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    if (confirmLabel != null) {
+                        CtaButton(
+                            modifier = Modifier.height(50.dp),
+                            labelString = confirmLabel,
+                            onClick = {
+                                onConfirm()
+                                onDismissRequest()
+                            },
+                            radius = 10,
+                            textStyle = MooiTheme.typography.mainButton,
+                        )
+                    }
+                    if (dismissLabel != null) {
+                        CtaButton(
+                            modifier = Modifier.height(50.dp),
+                            labelString = dismissLabel,
+                            onClick = {
+                                onDismiss()
+                                onDismissRequest()
+                            },
+                            type = CtaButtonType.TONAL,
+                            radius = 10,
+                            textStyle = MooiTheme.typography.mainButton,
+                        )
+                    }
                 }
             }
         }
