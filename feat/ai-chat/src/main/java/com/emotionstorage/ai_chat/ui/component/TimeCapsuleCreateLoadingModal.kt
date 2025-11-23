60320b4609e71fcd1d602df7e9b60a69f1138133
harry <harry7408@naver.com>
2025-11-23 19:48:00 +0900

    feat: TimeCapsule CreateLoading Modal

---
+++ feat/ai-chat/src/main/java/com/emotionstorage/ai_chat/ui/component/TimeCapsuleCreateLoadingModal.kt
@@ -0,0 +1,38 @@
+package com.emotionstorage.ai_chat.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.emotionstorage.ui.R
+import com.emotionstorage.ui.component.Modal
+import com.emotionstorage.ui.theme.MooiTheme
+
+@Composable
+fun TimeCapsuleCreateLoadingModal() {
+    Modal(
+        topDescription = "잠시만 기다려주세요.",
+        title = "지금 나눈 감정을\n타임캡슐에 담는 중이에요.",
+        onDismissRequest = { },
+        content = {
+            Box(modifier = Modifier.height(74.dp)) {
+                Image(
+                    painter = painterResource(R.drawable.gif_loading),
+                    contentDescription = "로딩중"
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun TimeCapsuleCreateLoadingModalPreview() {
+    MooiTheme {
+        TimeCapsuleCreateLoadingModal()
+    }
+}
