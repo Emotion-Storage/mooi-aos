7a6c4cb3e101c90967cd099c8afe8f1e7adcf6fd
sunjoolee <sheisthepoem@naver.com>
2025-11-28 17:41:35 +0900

    fix: fix time capsule detail api response
    set openAt as null

--- core/remote/src/main/java/com/emotionstorage/remote/response/timeCapsule/GetTimeCapsuleDetailResponse.kt
+++ core/remote/src/main/java/com/emotionstorage/remote/response/timeCapsule/GetTimeCapsuleDetailResponse.kt
@@ -6,6 +6,7 @@ import java.time.LocalDateTime
 @Serializable
 data class GetTimeCapsuleDetailResponse(
     val id: Long,
+    // 타임캡슐 대화 시작 시간
     @Serializable(with = LocalDateTimeSerializer::class)
     val historyDate: LocalDateTime,
     @Serializable(with = LocalDateTimeSerializer::class)
@@ -13,8 +14,9 @@ data class GetTimeCapsuleDetailResponse(
     @Serializable(with = LocalDateTimeSerializer::class)
     val updatedAt: LocalDateTime,
     val status: String,
+    // 타임캡슐 오픈일 - 임시저장 상태인 경우, null
     @Serializable(with = LocalDateTimeSerializer::class)
-    val openAt: LocalDateTime,
+    val openAt: LocalDateTime? = null,
     val isFavorite: Boolean,
     val title: String,
     val summary: String,
