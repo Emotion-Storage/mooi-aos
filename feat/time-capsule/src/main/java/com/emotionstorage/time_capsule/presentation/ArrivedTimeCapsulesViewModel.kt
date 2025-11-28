ff039df57e0d574fc82a37455c70d174eca7e3f1
sunjoolee <sheisthepoem@naver.com>
2025-11-28 10:55:34 +0900

    refactor: refactor favorite toggle & toast logic

--- feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/presentation/ArrivedTimeCapsulesViewModel.kt
+++ feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/presentation/ArrivedTimeCapsulesViewModel.kt
@@ -6,10 +6,8 @@ import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import androidx.paging.map
 import com.emotionstorage.domain.common.collectDataState
-import com.emotionstorage.domain.repo.FavoriteResult
 import com.emotionstorage.domain.useCase.timeCapsule.GetPagedArrivedTimeCapsulesUseCase
 import com.emotionstorage.domain.useCase.timeCapsule.SetFavoriteTimeCapsuleUseCase
-import com.emotionstorage.time_capsule.presentation.ArrivedTimeCapsulesSideEffect.ShowFavoriteToast
 import com.emotionstorage.time_capsule.ui.model.TimeCapsuleItemState
 import com.emotionstorage.time_capsule.ui.modelMapper.TimeCapsuleMapper
 import com.orhanobut.logger.Logger
@@ -21,30 +19,11 @@ import org.orbitmvi.orbit.annotation.OrbitExperimental
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
-sealed class ArrivedTimeCapsulesAction {
-    data class ToggleFavorite(
-        val id: Long,
-        val prevIsFavorite: Boolean,
-    ) : ArrivedTimeCapsulesAction()
-}
-
-sealed class ArrivedTimeCapsulesSideEffect {
-    data class ShowFavoriteToast(
-        val favoriteResult: FavoriteResult,
-    ) : ArrivedTimeCapsulesSideEffect()
-}
-
 @OptIn(OrbitExperimental::class)
 @HiltViewModel
 class ArrivedTimeCapsulesViewModel @Inject constructor(
     getArrivedTimeCapsules: GetPagedArrivedTimeCapsulesUseCase,
-    private val setFavorite: SetFavoriteTimeCapsuleUseCase,
-) : ViewModel(),
-    ContainerHost<Unit, ArrivedTimeCapsulesSideEffect> {
-    override val container =
-        container<Unit, ArrivedTimeCapsulesSideEffect>(Unit)
-
-    // paging data flow should not be managed by orbit!
+) : ViewModel() {
     val arrivedTimeCapsules: Flow<PagingData<TimeCapsuleItemState>> =
         getArrivedTimeCapsules()
             .map { pagingData ->
@@ -52,29 +31,4 @@ class ArrivedTimeCapsulesViewModel @Inject constructor(
                     TimeCapsuleMapper.toUi(it)
                 }
             }.cachedIn(viewModelScope)
-
-    fun onAction(action: ArrivedTimeCapsulesAction) {
-        when (action) {
-            is ArrivedTimeCapsulesAction.ToggleFavorite -> {
-                handleToggleFavorite(action.id, action.prevIsFavorite)
-            }
-        }
-    }
-
-    private fun handleToggleFavorite(
-        id: Long,
-        prevIsFavorite: Boolean,
-    ) = intent {
-        collectDataState(
-            flow = setFavorite(id, !prevIsFavorite),
-            onSuccess = {
-                // todo: integrate room paging using remote mediator
-                // ui will update automatically, on room entity change
-                postSideEffect(ShowFavoriteToast(it))
-            },
-            onError = { throwable, data ->
-                Logger.e("Failed to toggle favorite, $throwable")
-            },
-        )
-    }
 }
--- feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/ui/ArrivedTimeCapsulesScreen.kt
+++ feat/time-capsule/src/main/java/com/emotionstorage/time_capsule/ui/ArrivedTimeCapsulesScreen.kt
@@ -16,10 +16,12 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -28,43 +30,81 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
-import com.emotionstorage.time_capsule.presentation.ArrivedTimeCapsulesAction
-import com.emotionstorage.time_capsule.presentation.ArrivedTimeCapsulesSideEffect
 import com.emotionstorage.time_capsule.presentation.ArrivedTimeCapsulesViewModel
+import com.emotionstorage.time_capsule.presentation.ToggleFavoriteAction
+import com.emotionstorage.time_capsule.presentation.ToggleFavoriteSideEffect.ShowFavoriteFailToast
+import com.emotionstorage.time_capsule.presentation.ToggleFavoriteSideEffect.ShowFavoriteSuccessToast
+import com.emotionstorage.time_capsule.presentation.ToggleFavoriteState
+import com.emotionstorage.time_capsule.presentation.ToggleFavoriteViewModel
 import com.emotionstorage.time_capsule.ui.component.timeCapsuleItem.TimeCapsuleItem
 import com.emotionstorage.time_capsule.ui.model.TimeCapsuleItemState
 import com.emotionstorage.ui.R
 import com.emotionstorage.ui.component.toast.AppSnackbarHost
-import com.emotionstorage.ui.component.toast.FavoriteToast
 import com.emotionstorage.ui.component.appBar.TopAppBar
+import com.emotionstorage.ui.component.toast.AppSnackbarController
 import com.emotionstorage.ui.theme.MooiTheme
 
 @Composable
 fun ArrivedTimeCapsulesScreen(
     modifier: Modifier = Modifier,
     viewModel: ArrivedTimeCapsulesViewModel = hiltViewModel(),
+    favoriteViewModel: ToggleFavoriteViewModel = hiltViewModel(),
     navToTimeCapsuleDetail: (id: Long) -> Unit = {},
     navToBack: () -> Unit = {},
 ) {
+    val context = LocalContext.current
+
     val timeCapsulesState = viewModel.arrivedTimeCapsules.collectAsLazyPagingItems()
+    val favoriteState = favoriteViewModel.container.stateFlow.collectAsState()
 
     val snackState = remember { SnackbarHostState() }
+    val snackbarController = remember { AppSnackbarController(snackState) }
+
+    // update favorite state
+    LaunchedEffect(timeCapsulesState.itemSnapshotList) {
+        val favorites = timeCapsulesState
+            .itemSnapshotList.items
+            .filter { it.isFavorite }
+            .map { it.id }
+
+        favoriteViewModel.onAction(
+            ToggleFavoriteAction.Init(favorites)
+        )
+    }
+
+
+    // collect favorite side effect
     LaunchedEffect("init") {
-        viewModel.container.sideEffectFlow.collect {
+        favoriteViewModel.container.sideEffectFlow.collect {
             when (it) {
-                is ArrivedTimeCapsulesSideEffect.ShowFavoriteToast -> {
+                is ShowFavoriteSuccessToast -> {
                     snackState.currentSnackbarData?.dismiss()
-                    snackState.showSnackbar(it.favoriteResult.name)
+                    snackbarController.showSnackbar(
+                        message =
+                            if (it.isFavorite) context.getString(R.string.toast_favorite_added)
+                            else context.getString(R.string.toast_favorite_removed),
+                        iconResId = R.drawable.ic_success_filled,
+                    )
+                }
+
+                is ShowFavoriteFailToast -> {
+                    snackState.currentSnackbarData?.dismiss()
+                    snackbarController.showSnackbar(
+                        message = context.getString(R.string.toast_favorite_full),
+                    )
                 }
             }
         }
     }
 
+
     StatelessArrivedTimeCapsulesScreen(
         modifier = modifier,
         snackState = snackState,
+        snackbarController = snackbarController,
         timeCapsules = timeCapsulesState,
-        onAction = viewModel::onAction,
+        favoriteState = favoriteState.value,
+        onFavoriteAction = favoriteViewModel::onAction,
         navToTimeCapsuleDetail = navToTimeCapsuleDetail,
         navToBack = navToBack,
     )
@@ -74,8 +114,10 @@ fun ArrivedTimeCapsulesScreen(
 private fun StatelessArrivedTimeCapsulesScreen(
     modifier: Modifier = Modifier,
     snackState: SnackbarHostState = remember { SnackbarHostState() },
+    snackbarController: AppSnackbarController = remember { AppSnackbarController(SnackbarHostState()) },
     timeCapsules: LazyPagingItems<TimeCapsuleItemState>? = null,
-    onAction: (ArrivedTimeCapsulesAction) -> Unit = {},
+    favoriteState: ToggleFavoriteState = ToggleFavoriteState(),
+    onFavoriteAction: (ToggleFavoriteAction) -> Unit = {},
     navToTimeCapsuleDetail: (id: Long) -> Unit = {},
     navToBack: () -> Unit = {},
 ) {
@@ -94,9 +136,8 @@ private fun StatelessArrivedTimeCapsulesScreen(
         snackbarHost = {
             AppSnackbarHost(
                 hostState = snackState,
-            ) {
-                FavoriteToast(it.visuals.message)
-            }
+                customDataFlow = snackbarController.currentData
+            )
         },
     ) { innerPadding ->
         // todo: pull down to refresh
@@ -155,17 +196,16 @@ private fun StatelessArrivedTimeCapsulesScreen(
                                         Modifier
                                             .fillMaxWidth()
                                             .padding(bottom = 26.dp),
-                                    timeCapsule = this,
+                                    timeCapsule = this.copy(
+                                        isFavorite = favoriteState.favoriteIds.contains(this.id)
+                                    ),
                                     showDate = true,
                                     showInfoText = false,
                                     showFavorite = true,
                                     onClick = { navToTimeCapsuleDetail(this.id) },
                                     onFavoriteClick = {
-                                        onAction(
-                                            ArrivedTimeCapsulesAction.ToggleFavorite(
-                                                this.id,
-                                                this.isFavorite,
-                                            ),
+                                        onFavoriteAction(
+                                            ToggleFavoriteAction.OnToggle(this.id),
                                         )
                                     },
                                 )
