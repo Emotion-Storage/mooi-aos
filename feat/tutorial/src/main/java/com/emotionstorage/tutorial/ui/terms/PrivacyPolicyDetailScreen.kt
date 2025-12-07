ef42615c06a20d5ca0987ad401adcf9d2ee8f6f2
sunjoolee <sheisthepoem@naver.com>
2025-12-07 11:50:08 +0900

    chore: move dir

--- feat/tutorial/src/main/java/com/emotionstorage/tutorial/ui/terms/PrivacyPolicyDetailScreen.kt
+++ feat/tutorial/src/main/java/com/emotionstorage/tutorial/ui/terms/PrivacyPolicyDetailScreen.kt
@@ -1,4 +1,4 @@
-package com.emotionstorage.tutorial.ui.onBoarding
+package com.emotionstorage.tutorial.ui.terms
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
