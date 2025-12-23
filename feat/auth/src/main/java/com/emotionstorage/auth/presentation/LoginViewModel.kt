95c7575f2348440cd12aedc408c0c8817156b8be
sunjoolee <sheisthepoem@naver.com>
2025-12-23 11:17:24 +0900

    fix: fix network error logic

--- feat/auth/src/main/java/com/emotionstorage/auth/presentation/LoginViewModel.kt
+++ feat/auth/src/main/java/com/emotionstorage/auth/presentation/LoginViewModel.kt
@@ -60,8 +60,7 @@ class LoginViewModel
                         reduce {
                             state.copy(isLoading = false)
                         }
-                        // TODO : OR로 2번째로 붙는 조건 관련 로직 수정 필요 (온보딩으로 넘어가지 않는 이슈 임시 해결)
-                        if (code == ErrorCode.NEED_SIGN_UP || code == ErrorCode.NETWORK_ERROR) {
+                        if (code == ErrorCode.NEED_SIGN_UP) {
                             val idToken =
                                 (data as? String) ?: throw BaseException(
                                     message = "idToken is null or not a String",
