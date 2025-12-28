9269ace61ee31a10a63f8833038bdcbb7c071f54
sunjoolee <sheisthepoem@naver.com>
2025-12-28 20:56:56 +0900

    feat: independent retrofit module for auth api service

---
+++ core/remote/src/main/java/com/emotionstorage/remote/di/AuthApiModule.kt
@@ -0,0 +1,55 @@
+package com.emotionstorage.remote.di
+
+import com.emotionstorage.remote.BuildConfig
+import com.emotionstorage.remote.api.AuthApiService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthApiModule {
+    private const val BASE_URL = "http://${BuildConfig.MOOI_DEV_SERVER_URL}"
+    private const val TIMEOUT = 20L
+
+    private val json =
+        Json {
+            ignoreUnknownKeys = true // Common configuration
+            isLenient = true
+            prettyPrint = true // For debugging, optional
+        }
+
+    /**
+     * auth retrofit to prevent dependency cycle
+     */
+    @Provides
+    @Singleton
+    @Named("AuthRetrofit")
+    fun provideAuthRetrofit(
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .client(
+                OkHttpClient.Builder()
+                    .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+                    .readTimeout(TIMEOUT, TimeUnit.SECONDS)
+                    .build())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthApi(
+        @Named("AuthRetrofit") retrofit: Retrofit
+    ): AuthApiService = retrofit.create(AuthApiService::class.java)
+}
