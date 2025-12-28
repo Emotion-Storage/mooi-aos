0ac85fa20dd6fc050b9d1945f9983b538cdd106d
sunjoolee <sheisthepoem@naver.com>
2025-12-28 14:07:12 +0900

    chore: move dir

--- feat/auth/src/main/java/com/emotionstorage/auth/ui/modal/RetryLoginModal.kt
+++ feat/auth/src/main/java/com/emotionstorage/auth/ui/modal/RetryLoginModal.kt
@@ -1,9 +1,16 @@
-package com.emotionstorage.tutorial.ui.modal
+package com.emotionstorage.auth.ui.modal
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.emotionstorage.ui.component.modal.Modal
 
+// todo: 알럿 정의서에 따라 코멘트 내용 수정하기
+/**
+ * 로그인 재시도 팝업
+ * 회원가입 완료 화면에서, 회원가입 성공 but 로그인 실패한 경우 표시
+ * [다시 로그인하기] -> 로그인 화면으로 이동
+ * 배경 클릭 X, 뒤로가기 X
+ */
 @Composable
 fun RetryLoginModal(
     onConfirm: () -> Unit,
