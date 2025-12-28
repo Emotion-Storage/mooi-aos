2794b66151b5cdfe3445d4c2d205ccf31102f82b
sunjoolee <sheisthepoem@naver.com>
2025-12-28 23:07:07 +0900

    feat: save new access token in TokenAuthenticator

--- core/remote/src/main/java/com/emotionstorage/remote/interceptor/TokenAuthenticator.kt
+++ core/remote/src/main/java/com/emotionstorage/remote/interceptor/TokenAuthenticator.kt
@@ -1,8 +1,13 @@
 package com.emotionstorage.remote.interceptor
 
+import com.emotionstorage.data.dataSource.local.SessionLocalDataSource
+import com.emotionstorage.data.model.SessionEntity
 import com.emotionstorage.remote.api.AuthApiService
 import com.emotionstorage.remote.api.ReissueApiService
 import com.orhanobut.logger.Logger
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import okhttp3.Authenticator
 import okhttp3.Request
@@ -11,16 +16,26 @@ import okhttp3.Route
 import javax.inject.Inject
 
 class TokenAuthenticator @Inject constructor(
-    private val reissueApi: ReissueApiService
+    private val reissueApi: ReissueApiService,
+    private val sessionLocalDataSource: SessionLocalDataSource,
 ) : Authenticator {
 
     override fun authenticate(route: Route?, response: Response): Request? {
+        // prevent infinite loop
         if (responseCount(response) >= 2) return null
 
         try {
             val newAccessToken = runBlocking {
                 reissueApi.postReissue()
-            }.data?.accessToken ?: return null
+            }.data?.accessToken
+            if (newAccessToken == null) return null
+
+            // save new access token & retry request
+            CoroutineScope(Dispatchers.IO).launch {
+                sessionLocalDataSource.saveSession(
+                    SessionEntity(newAccessToken)
+                )
+            }
             return response.request.newBuilder()
                 .header("Authorization", "Bearer $newAccessToken")
                 .build()
