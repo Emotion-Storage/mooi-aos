3c78c8f277ff4a514848dff1865a1f57a4ca1e48
sunjoolee <sheisthepoem@naver.com>
2025-12-29 10:23:56 +0900

    style: format code

--- core/ui/src/main/java/com/emotionstorage/ui/annotation/PreviewDevices.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/annotation/PreviewDevices.kt
@@ -4,45 +4,44 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.FUNCTION)
-
-// 최소 대응
 @Preview(
+    // 최소 대응
     name = "Android Small",
     showBackground = true,
     widthDp = 360,
     heightDp = 640,
 )
-// 기준 디바이스
 @Preview(
+    // 기준 디바이스
     name = "Android Normal",
     showBackground = true,
     widthDp = 411,
     heightDp = 891,
 )
-// 큰 폰 대응
 @Preview(
+    // 큰 폰 대응
     name = "Android Large",
     showBackground = true,
     widthDp = 412,
     heightDp = 915,
 )
-// Z Flip 대응
-// Z Flip 펼친 크기 = Z Fold 접힌 크기와 거의 동일
 @Preview(
+    // Z Flip 대응
+    // Z Flip 펼친 크기 = Z Fold 접힌 크기와 거의 동일
     name = "Z Flip",
     showBackground = true,
     widthDp = 360,
     heightDp = 748,
 )
-// Z Fold 대응
 @Preview(
+    // Z Fold 대응
     name = "Z Fold",
     showBackground = true,
     widthDp = 673,
     heightDp = 841,
 )
-// 태블릿 대응
 @Preview(
+    // 태블릿 대응
     name = "Tablet",
     showBackground = true,
     widthDp = 800,
