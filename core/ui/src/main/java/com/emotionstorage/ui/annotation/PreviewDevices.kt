1fa682d9cdc818b2b38cfb371d1c78ede50dd317
sunjoolee <sheisthepoem@naver.com>
2025-12-29 10:37:42 +0900

    fix: fix device dp values

--- core/ui/src/main/java/com/emotionstorage/ui/annotation/PreviewDevices.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/annotation/PreviewDevices.kt
@@ -1,50 +1,37 @@
 package com.emotionstorage.ui.annotation
 
+import androidx.compose.ui.tooling.preview.Devices.PIXEL_4
+import androidx.compose.ui.tooling.preview.Devices.PIXEL_7_PRO
+import androidx.compose.ui.tooling.preview.Devices.TABLET
 import androidx.compose.ui.tooling.preview.Preview
 
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.FUNCTION)
 @Preview(
-    // 최소 대응
     name = "Android Small",
     showBackground = true,
-    widthDp = 360,
-    heightDp = 640,
+    widthDp = 320,
+    heightDp = 480,
 )
 @Preview(
-    // 기준 디바이스
     name = "Android Normal",
     showBackground = true,
-    widthDp = 411,
-    heightDp = 891,
+    device = PIXEL_4,
 )
 @Preview(
-    // 큰 폰 대응
     name = "Android Large",
     showBackground = true,
-    widthDp = 412,
-    heightDp = 915,
+    device = PIXEL_7_PRO,
 )
 @Preview(
-    // Z Flip 대응
-    // Z Flip 펼친 크기 = Z Fold 접힌 크기와 거의 동일
-    name = "Z Flip",
-    showBackground = true,
+    name = "Z Flip 5 - Open",
     widthDp = 360,
     heightDp = 748,
 )
 @Preview(
-    // Z Fold 대응
-    name = "Z Fold",
+    name = "Z Fold 5 - Open",
     showBackground = true,
     widthDp = 673,
     heightDp = 841,
-)
-@Preview(
-    // 태블릿 대응
-    name = "Tablet",
-    showBackground = true,
-    widthDp = 800,
-    heightDp = 1280,
 )
 annotation class PreviewDevices
