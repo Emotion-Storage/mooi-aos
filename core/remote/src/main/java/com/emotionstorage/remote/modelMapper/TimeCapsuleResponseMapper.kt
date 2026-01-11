8595badabaf76f0617ee0b5ee24997d68d307ec3
sunjoolee <sheisthepoem@naver.com>
2026-01-11 12:38:20 +0900

    fix: fix time capsule entity mapping

--- core/remote/src/main/java/com/emotionstorage/remote/modelMapper/TimeCapsuleResponseMapper.kt
+++ core/remote/src/main/java/com/emotionstorage/remote/modelMapper/TimeCapsuleResponseMapper.kt
@@ -1,7 +1,6 @@
 package com.emotionstorage.remote.modelMapper
 
 import com.emotionstorage.data.model.TimeCapsuleEntity
-import com.emotionstorage.data.model.TimeCapsuleEntity.PageData
 import com.emotionstorage.remote.response.timeCapsule.GetTimeCapsuleDetailResponse
 import com.emotionstorage.remote.response.timeCapsule.GetTimeCapsulesResponse
 
@@ -23,11 +22,6 @@ internal object TimeCapsuleResponseMapper {
                 createdAt = it.createdAt,
                 openAt = it.openAt,
                 updatedAt = it.updatedAt,
-                pageData =
-                    PageData(
-                        page = response.pagination.page,
-                        hasNextPage = response.pagination.page < response.pagination.totalPage,
-                    ),
             )
         }
 
