b40691dba9cd699f86f4ebf14ad32b3e4f758f21
harry <harry7408@naver.com>
2026-01-11 15:05:28 +0900

    feat: divide coroutine cancellationException and normal Exception

--- core/local/src/main/java/com/emotionstorage/local/dataSourceImpl/AttendanceClaimLocalDataSourceImpl.kt
+++ core/local/src/main/java/com/emotionstorage/local/dataSourceImpl/AttendanceClaimLocalDataSourceImpl.kt
@@ -2,33 +2,42 @@ package com.emotionstorage.local.dataSourceImpl
 
 import android.content.Context
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.emotionstorage.data.dataSource.local.AttendanceClaimLocalDataSource
+import com.orhanobut.logger.Logger
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import okio.IOException
 import javax.inject.Inject
+
+private val Context.attendanceDataStore by preferencesDataStore(name = "attendance_prefs")
 
 class AttendanceClaimLocalDataSourceImpl @Inject constructor(
     @ApplicationContext private val context: Context,
 ) : AttendanceClaimLocalDataSource {
-    private val Context.attendanceDataStore by preferencesDataStore(name = "attendance_prefs")
-
     override suspend fun getLastClaimDate(): String? =
-        try {
-            context.attendanceDataStore.data.first()[KEY_LAST_CLAIM_DATE]
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
-        }
+        context
+            .attendanceDataStore
+            .data
+            .catch { e ->
+                if (e is IOException) emit(emptyPreferences()) else throw e
+            }.map { prefs -> prefs[KEY_LAST_CLAIM_DATE] }
+            .first()
 
     override suspend fun setLastClaimDate(date: String) {
         try {
             context.attendanceDataStore.edit { prefs ->
                 prefs[KEY_LAST_CLAIM_DATE] = date
             }
+        } catch (e: CancellationException) {
+            throw e
         } catch (e: Exception) {
-            e.printStackTrace()
+            Logger.e("Failed setLastClaimDate")
         }
     }
 
@@ -37,8 +46,10 @@ class AttendanceClaimLocalDataSourceImpl @Inject constructor(
             context.attendanceDataStore.edit { prefs ->
                 prefs.remove(KEY_LAST_CLAIM_DATE)
             }
+        } catch (e: CancellationException) {
+            throw e
         } catch (e: Exception) {
-            e.printStackTrace()
+            Logger.e("Failed clear preferences")
         }
     }
 
