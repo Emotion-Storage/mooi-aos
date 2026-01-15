e10be3543f3d6722907d4747db6353e0f09fe387
sunjoolee <sheisthepoem@naver.com>
2026-01-15 10:28:56 +0900

    feat: update capsule_save_exit modal ui

--- feat/time-capsule-detail/src/main/java/com/emotionstorage/time_capsule_detail/ui/modal/ExitTempTimeCapsuleModal.kt
+++ feat/time-capsule-detail/src/main/java/com/emotionstorage/time_capsule_detail/ui/modal/ExitTempTimeCapsuleModal.kt
@@ -1,28 +1,22 @@
 package com.emotionstorage.time_capsule_detail.ui.modal
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.emotionstorage.ui.component.modal.Modal
-import com.emotionstorage.ui.theme.MooiTheme
 
+/**
+ * capsule_save_exit
+ * - Case : 타임캡슐 보관 중 나가기(임시저장 confirm)
+ *     - 타임캡슐 생성 후 상세페이지에서 뒤로가기를 시도했을 때 표출
+ * - 표시 화면
+ *     - 3.5 타임캡슐 - 생성된 타임캡슐
+ * - CTA 및 이동
+ *     - [아니요, 계속할래요.] -> 팝업 닫히며 이동 X
+ *     - [네, 그냥 나갈래요.] -> 팝업 닫히며 메인 화면으로 이동
+ * - 차단
+ *     - 뒤로가기(Android) O (팝업 닫힘, 화면 제자리)
+ *     - 배경 터치 O (팝업 닫힘, 화면 제자리)
+ */
 @Composable
 fun ExitTempTimeCapsuleModal(
     onDismissRequest: () -> Unit = {},
@@ -30,7 +24,9 @@ fun ExitTempTimeCapsuleModal(
 ) {
     Modal(
         onDismissRequest = onDismissRequest,
-        contentPadding = PaddingValues(top = 23.dp, bottom = 28.dp, start = 25.dp, end = 25.dp),
+        topDescription = "지금 나가면 임시 저장돼요.\n24시간 안에 다시 보관할 수 있어요.",
+        topDescriptionHighlights = listOf("24시간"),
+        title = "페이지를 나가시겠어요?",
         confirmLabel = "아니요, 계속할래요.",
         onConfirm = {
             // do nothing on dismiss
@@ -38,66 +34,5 @@ fun ExitTempTimeCapsuleModal(
         dismissLabel = "네, 그냥 나갈래요.",
         onDismiss = onExit,
         modalWidth = 298.dp,
-    ) {
-        ModalContent()
-    }
-}
-
-@Composable
-private fun ModalContent(modifier: Modifier = Modifier) {
-    Column(
-        modifier =
-            modifier
-                .fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(
-            text =
-                buildAnnotatedString {
-                    append("타임캡슐을 보관하지 않으면,\n")
-                    append("감정은 임시 저장되며\n")
-                    withStyle(
-                        SpanStyle(
-                            color = MooiTheme.colorScheme.primaryBlue500,
-                        ),
-                    ) {
-                        append("24시간 후 자동 삭제")
-                    }
-                    append("돼요.")
-                },
-            style = MooiTheme.typography.body1.copy(lineHeight = 22.sp),
-            color = MooiTheme.colorScheme.gray300,
-            textAlign = TextAlign.Center,
-        )
-        Text(
-            modifier = Modifier.padding(top = 9.dp, bottom = 15.dp),
-            text = "* 그 전까지는 언제든 다시 보관할 수 있어요.",
-            style = MooiTheme.typography.body8,
-            color = MooiTheme.colorScheme.gray500,
-        )
-        Text(
-            modifier = Modifier.height(30.dp),
-            text = "지금 페이지를 나가시겠어요?",
-            style = MooiTheme.typography.head2,
-            color = Color.White,
-        )
-    }
-}
-
-@Preview
-@Composable
-private fun ExitTempTimeCapsuleModalContentPreview() {
-    ModalContent(
-        modifier =
-            Modifier
-                .width(298.dp)
-                .background(MooiTheme.colorScheme.backgroundDefault)
-                .padding(
-                    top = 23.dp,
-                    bottom = 18.dp,
-                    start = 25.dp,
-                    end = 25.dp,
-                ),
     )
 }
