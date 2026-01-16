f22ab28c9d5081ae9a9efd9133f9d18a47dd2da1
sunjoolee <sheisthepoem@naver.com>
2026-01-16 17:26:32 +0900

    wip: push notification screen view model

--- feat/alarm/src/main/java/com/emotionstorage/alarm/presentation/PushNotificationViewModel.kt
+++ feat/alarm/src/main/java/com/emotionstorage/alarm/presentation/PushNotificationViewModel.kt
@@ -2,52 +2,88 @@ package com.emotionstorage.alarm.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.emotionstorage.domain.common.DataState
 import com.emotionstorage.domain.model.Notification
-import com.emotionstorage.domain.model.NotificationType
+import com.emotionstorage.domain.useCase.dailyReport.GetDailyReportByIdUseCase
+import com.emotionstorage.domain.useCase.notification.GetPagedNotificationsUseCase
+import com.emotionstorage.domain.useCase.timeCapsule.GetTimeCapsuleByIdUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
-import java.time.LocalDateTime
+import kotlinx.coroutines.flow.Flow
+import org.orbitmvi.orbit.Container
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
-@HiltViewModel
-class PushNotificationViewModel @Inject constructor() : ViewModel() {
-    private val _state = MutableStateFlow<List<Notification>>(emptyList())
-    val state: StateFlow<List<Notification>> = _state
+data class PushNotificationState(
+    val isLoading: Boolean = true,
+)
 
-    init {
-        // TODO : Mock Data 추후 삭제
-        loadData(useMock = true)
+sealed class PushNotificationSideEffect {
+    data class GetDailyReportDetailSuccess(
+        val id: Long,
+    ) : PushNotificationSideEffect()
+
+    data class GetTimeCapsuleDetailSuccess(
+        val id: Long,
+    ) : PushNotificationSideEffect()
+
+    object GetDetailError : PushNotificationSideEffect()
+}
+
+sealed class PushNotificationAction {
+    data class GetDailyReportDetail(val id: Long) : PushNotificationAction()
+    data class GetTimeCapsuleDetail(val id: Long) : PushNotificationAction()
+}
+
+@HiltViewModel
+class PushNotificationViewModel @Inject constructor(
+    private val getPagedNotifications: GetPagedNotificationsUseCase,
+    private val getDailyReportById: GetDailyReportByIdUseCase,
+    private val getTimeCapsuleById: GetTimeCapsuleByIdUseCase,
+) : ViewModel(), ContainerHost<PushNotificationState, PushNotificationSideEffect> {
+    override val container = container<PushNotificationState, PushNotificationSideEffect>(PushNotificationState())
+
+    val notifications: Flow<PagingData<Notification>> =
+        getPagedNotifications().cachedIn(viewModelScope)
+
+    fun onAction(action: PushNotificationAction) {
+        when (action) {
+            is PushNotificationAction.GetDailyReportDetail -> {
+                handleGetDailyReportDetail(action.id)
+            }
+
+            is PushNotificationAction.GetTimeCapsuleDetail -> {
+                handleGetTimeCapsuleDetail(action.id)
+            }
+        }
     }
 
-    fun loadData(useMock: Boolean) {
-        viewModelScope.launch {
-            _state.value = if (useMock) mockItems() else emptyList()
+    private fun handleGetDailyReportDetail(id: Long) = intent {
+        reduce { state.copy(isLoading = true) }
+        getDailyReportById(id).handle(
+            onSuccess = {
+                reduce { state.copy(isLoading = false) }
+                postSideEffect(PushNotificationSideEffect.GetDailyReportDetailSuccess(it.id))
+            },
+            onError = { throwable, code, data ->
+                reduce { state.copy(isLoading = false) }
+                postSideEffect(PushNotificationSideEffect.GetDetailError)
+            },
+        )
+    }
+
+    private fun handleGetTimeCapsuleDetail(id: Long) = intent {
+        reduce { state.copy(isLoading = true) }
+        getTimeCapsuleById(id).collect {
+            if (it is DataState.Success) {
+                reduce { state.copy(isLoading = false) }
+                postSideEffect(PushNotificationSideEffect.GetTimeCapsuleDetailSuccess(it.data.id))
+            } else if (it is DataState.Error) {
+                reduce { state.copy(isLoading = false) }
+                postSideEffect(PushNotificationSideEffect.GetDetailError)
+            }
         }
     }
 }
-
-private fun mockItems() =
-    listOf(
-        Notification(
-            id = 0,
-            type = NotificationType.RecordSchedule,
-            arrivedAt = LocalDateTime.now().minusHours(1),
-        ),
-        Notification(
-            id = 0,
-            type = NotificationType.DailyReportArrival(1),
-            arrivedAt = LocalDateTime.now().minusHours(3),
-        ),
-        Notification(
-            id = 0,
-            type = NotificationType.TimeCapsuleArrival(1),
-            arrivedAt = LocalDateTime.now().minusDays(0),
-        ),
-        Notification(
-            id = 0,
-            type = NotificationType.RecordReminder,
-            arrivedAt = LocalDateTime.now().minusDays(3),
-        ),
-    )
