34a7f7d8e502a77ddea553aa31b09eb08195a723
sunjoolee <sheisthepoem@naver.com>
2026-01-16 18:00:12 +0900

    fix: fix lazy column alignment

--- feat/alarm/src/main/java/com/emotionstorage/alarm/ui/PushNotificationScreen.kt
+++ feat/alarm/src/main/java/com/emotionstorage/alarm/ui/PushNotificationScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -116,15 +117,19 @@ private fun StatelessPushNotificationScreen(
                     .padding(innerPadding),
         ) {
             LazyColumn(
+                modifier = Modifier.fillMaxSize(),
                 contentPadding = PaddingValues(start = 16.dp, end = 16.dp, top = 12.dp),
                 verticalArrangement = Arrangement.spacedBy(13.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 item {
                     Row(
                         modifier =
                             Modifier
+                                .fillMaxWidth()
                                 .height(24.dp),
                         verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Start,
                     ) {
                         Icon(
                             modifier =
