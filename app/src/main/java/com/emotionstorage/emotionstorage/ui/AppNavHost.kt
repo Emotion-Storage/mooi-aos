f852c3c73e509cd488e6129a0e538de93132528c
sunjoolee <sheisthepoem@naver.com>
2026-01-28 11:39:24 +0900

    feat: add temp & session expired modal in arrived screen

--- app/src/main/java/com/emotionstorage/emotionstorage/ui/AppNavHost.kt
+++ app/src/main/java/com/emotionstorage/emotionstorage/ui/AppNavHost.kt
@@ -379,6 +379,9 @@ internal fun AppNavHost(
                 navToBack = {
                     navController.popBackStack()
                 },
+                navToLogin = {
+                    navController.navigateAsRoot(AppDestination.Login)
+                },
             )
         }
 
