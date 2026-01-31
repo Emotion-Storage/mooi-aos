be965307803389204132c874752a483b81ab3b7f
harry <harry7408@naver.com>
2026-01-31 14:20:25 +0900

    ui: showbackground remove

--- core/ui/src/main/java/com/emotionstorage/ui/annotation/PreviewScreenRatios.kt
+++ core/ui/src/main/java/com/emotionstorage/ui/annotation/PreviewScreenRatios.kt
@@ -11,11 +11,11 @@ import androidx.compose.ui.tooling.preview.Preview
  */
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.FUNCTION)
-@Preview(name = "Ratio 9:20 (Standard)", widthDp = 412, heightDp = 915, showBackground = true, showSystemUi = true)
-@Preview(name = "Ratio 9:16 (Old/Small)", widthDp = 360, heightDp = 640, showBackground = true, showSystemUi = true)
-@Preview(name = "Ratio 9:23 (Long/Thin)", widthDp = 360, heightDp = 920, showBackground = true, showSystemUi = true)
-@Preview(name = "Ratio 5:6 (Square-ish)", widthDp = 775, heightDp = 931, showBackground = true, showSystemUi = true)
-@Preview(name="Phone • MinWidth (320x700dp)", widthDp=320, heightDp=700, showSystemUi=true, showBackground=true)
-@Preview(name="Phone • Mid (384x854dp)", widthDp=384, heightDp=854, showSystemUi=true, showBackground=true)
-@Preview(name="Design Base (360x800dp)", widthDp=360, heightDp=800, showSystemUi=true, showBackground=true)
+@Preview(name = "Ratio 9:20 (Standard)", widthDp = 412, heightDp = 915, showBackground = true)
+@Preview(name = "Ratio 9:16 (Old/Small)", widthDp = 360, heightDp = 640, showBackground = true)
+@Preview(name = "Ratio 9:23 (Long/Thin)", widthDp = 360, heightDp = 920, showBackground = true)
+@Preview(name = "Ratio 5:6 (Square-ish)", widthDp = 775, heightDp = 931, showBackground = true)
+@Preview(name = "Phone • MinWidth (320x700dp)", widthDp = 320, heightDp = 700, showSystemUi = true)
+@Preview(name = "Phone • Mid (384x854dp)", widthDp = 384, heightDp = 854, showSystemUi = true)
+@Preview(name = "Design Base (360x800dp)", widthDp = 360, heightDp = 800, showSystemUi = true)
 annotation class PreviewScreenRatios
