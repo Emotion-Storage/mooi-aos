332ac93a99aa8b710e8ad2131a5fd289b8cb5221
sunjoolee <sheisthepoem@naver.com>
2026-05-07 21:37:00 +0900

    fix: fix retrofit server base url

--- core/remote/src/main/java/com/emotionstorage/remote/di/ReissueApiServiceModule.kt
+++ core/remote/src/main/java/com/emotionstorage/remote/di/ReissueApiServiceModule.kt
@@ -20,7 +20,8 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object ReissueApiServiceModule {
     private val BASE_URL =
-        "http://" + (if (BuildConfig.DEBUG) BuildConfig.MOOI_DEV_SERVER_URL else BuildConfig.MOOI_PROD_SERVER_URL)
+        if (BuildConfig.DEBUG) ("http://" + BuildConfig.MOOI_DEV_SERVER_URL)
+        else ("https://" + BuildConfig.MOOI_PROD_SERVER_URL)
     private const val TIMEOUT = 20L
 
     /**
