930ed867797f1ce789398ab44770e3e1ef464e0c
sunjoolee <sheisthepoem@naver.com>
2026-05-07 21:17:14 +0900

    build: update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
 
     defaultConfig {
         applicationId = "com.emotionstorage.emotionstorage"
-        versionCode = 7
-        versionName = "1.0.1"
+        versionCode = 8
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
